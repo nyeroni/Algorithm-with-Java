e6de43ed66515cbbaccdc32e18c08d7be656e9c4
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 20:47:04 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    long int n;
+    cin>>n;
+    cout<<n*n*n<<endl;
+    cout<<"3"<<endl;
+    return 0;
+}
+
