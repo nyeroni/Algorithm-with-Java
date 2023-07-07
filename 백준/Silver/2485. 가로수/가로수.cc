5fa021037e22421ffb9c7cf5d7131c0b763c9637
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 20:56:05 +0900

    [Silver IV] Title: 가로수, Time: 12 ms, Memory: 3188 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.cc
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<iterator>
+#include<map>
+#include<set>
+using namespace std;
+int GCD(int a, int b){
+    int c=a%b;
+    while(c!=0){
+        a=b;
+        b=c;
+        c=a%b;
+    }
+    return b;
+}
+int LCM(int a, int b){
+    return a*b/GCD(a,b);
+}
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    vector<int>v;
+    cin>>N;
+    int arr[N];
+    for(int i=0; i<N; i++){
+        int x;
+        cin>>arr[i];
+    }
+    sort(arr, arr+N);
+    for(int i=1; i<N; i++){
+        v.push_back(arr[i]-arr[i-1]);
+    }
+    int tmp=v[0];
+    for(int i=1; i<v.size(); i++){
+        tmp=GCD(tmp, v[i]);
+    }
+    int cnt=0;
+    for(int i=0; i<v.size(); i++){
+        cnt+=v[i]/tmp-1;
+    }
+    cout<<cnt<<'\n';
+    return 0;
+}
