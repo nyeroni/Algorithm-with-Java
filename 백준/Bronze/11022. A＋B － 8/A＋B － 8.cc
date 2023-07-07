26a5e253a6902db6b9b8861e3aa3fdb183f5f8da
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 20:26:44 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N,A,B;
+    cin>>N;
+    for(int i=0; i<N; i++){
+        cin>>A>>B;
+        cout<<"Case #"<<i+1<<": "<<A<<" + "<<B<<" = "<<A+B<<'\n';
+    }
+    return 0;
+
+}
