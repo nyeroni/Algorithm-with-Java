1dd56f89d283f308dba304cf069ff7baaba8a07f
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 20:49:53 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 44 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+bool cmp(int a, int b){
+    return a>b;
+}
+typedef struct xy{
+   int x,y;
+}xy;
+
+bool compare(xy a, xy b){
+    if(a.x==b.x)return a.y<b.y;
+    return a.x<b.x;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    xy z[100000];
+    for(int i=0; i<N; i++){
+        cin>>z[i].x>>z[i].y;
+    }
+    sort(z, z+N, compare);
+    for(int i=0; i<N; i++){
+        cout<<z[i].x<<" "<<z[i].y<<"\n";
+    }
+    return 0;
+}
+
