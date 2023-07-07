ae47e2b9940629bfdbfbd73e599ee7cff3d126a7
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 20:25:02 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    int total=0;
+    cin>>N;
+    for(int i=1; i<=N; i++){
+        total+=i;
+    }
+    cout<<total<<endl;
+    return 0;
+
+}
