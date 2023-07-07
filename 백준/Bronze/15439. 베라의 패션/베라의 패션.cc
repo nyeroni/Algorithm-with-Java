44d4f017ff38aec4cc63b60c36f88ab4677ec836
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 21:14:02 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<iterator>
+#include<map>
+#include<set>
+using namespace std;
+int arr[1000001]={0};
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin>>N;
+    cout<<N*N-N<<'\n';
+    return 0;
+}
