666c608e1e56fcddc9e5c75ba41ae3d3ec979162
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 20:20:38 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cin>>a>>b;
+    cout.precision(9);
+    cout<<fixed;
+    cout<<(double)a/(double)b<<endl;
+    return 0;
+}
