3f9b330a9cc8912abb78c714196711665f14cffc
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 20:23:00 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,7 @@
+#include<iostream>  //헤더파일 포함
+using namespace std;
+int main() //main() 함수의 시작
+{
+	cout<<"|\\_/|\n"<<"|q p|   /}\n"<<"( 0 )\"\"\"\\\n"<<"|\"^\"`    |\n"<<"||_/=\\\\__|\n";
+	return 0; //0을 반환
+}  
