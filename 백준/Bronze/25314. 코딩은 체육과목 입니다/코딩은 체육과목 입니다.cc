2de4b7d9b57e8ab68d6959f98e1bc5a64dedfac9
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 20:25:30 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N, tmp;
+    cin>>N;
+    tmp=N/4;
+    for(int i=0; i<tmp; i++){
+        cout<<"long ";
+    }
+    cout<<"int"<<endl;
+    return 0;
+
+}
