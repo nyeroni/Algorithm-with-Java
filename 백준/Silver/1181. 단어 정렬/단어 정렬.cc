9d81fdad9e6beb2462a5d3212a4ae6e43adeaf59
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-07-07 20:50:17 +0900

    [Silver V] Title: 단어 정렬, Time: 44 ms, Memory: 3856 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+bool cmp(int a, int b){
+    return a>b;
+}
+bool compare(string a, string b){
+    if(a.length()==b.length()){
+        return a<b;
+    }
+    else{
+        return a.length()<b.length();
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N;
+    cin>>N;
+    string arr[20000];
+
+    for(int i=0; i<N; i++){
+        cin>>arr[i];
+    }
+    sort(arr, arr+N, compare);
+    for(int i=0; i<N; i++){
+        if(arr[i]==arr[i-1])continue;
+        else cout<<arr[i]<<'\n';
+    }
+    return 0;
+}
+
