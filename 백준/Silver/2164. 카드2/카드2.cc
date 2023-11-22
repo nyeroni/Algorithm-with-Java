efc241fddbc761d7e5e6b0ca07c2a9fd45aac018
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2023-11-22 12:22:34 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -1,40 +1,27 @@
 #include<iostream>
-#include<string>
-#include<cmath>
-#include<algorithm>
-#include<vector>
-#include<iterator>
-#include<map>
-#include<set>
-#include<unordered_set>
-#include <stack>
 #include<queue>
-using namespace std;
+#include<algorithm>
+using namespace  std;
 
-int main() {
-
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     int N;
+
     cin>>N;
-    queue<int>q;
-    if(N==1){
-        cout<<"1\n";
-        return 0;
-    }
+    queue<int> q;
+
     for(int i=1; i<=N; i++){
         q.push(i);
     }
-    while(true){
-        if(q.front()==q.back())break;
+    while(q.size()!=1){
+        q.pop();
+        q.push(q.front());
         q.pop();
 
-        int k=q.front();
-        q.push(k);
-        q.pop();
     }
-    cout<<q.front()<<'\n';
+    cout<<q.front();
     return 0;
 }
