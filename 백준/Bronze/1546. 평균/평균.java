5fed998f6c6372d4644f2c0363eee545ee375ff2
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-09-02 16:21:07 +0900

    [Bronze I] Title: 평균, Time: 208 ms, Memory: 18556 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,23 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-   public static void main(String [] args) {
 
-       Scanner sc = new Scanner(System.in);
-       int M = sc.nextInt();
-       int max = 0;
-       int[] arr = new int[1001];
-       double sum = 0;
-       for (int i = 0; i < M; i++) {
-           arr[i] = sc.nextInt();
-           if (arr[i] > max) {
-               max = arr[i];
-           }
-       }
+    public static void main(String [] args) {
 
-       for (int i = 0; i < M; i++) {
-           sum += ((double) arr[i]/max)*100;
-       }
-       System.out.println(sum/(double) M);
-   }
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int [] arr = new int[N];
+        int max = 0;
+        double sum = 0;
+        double avg = 0;
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            sum += (double) arr[i] / max * 100;
+        }
+
+        avg = sum/(double) N;
+
+        System.out.println(avg);
+    }
 }
