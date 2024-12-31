1f98178774147992a718486fd87086820613711c
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-12-31 16:59:18 +0900

    [Silver II] Title: DNA 비밀번호, Time: 272 ms, Memory: 24904 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,79 +1,104 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    private static int[] arr = {0, 0, 0, 0};
-    public static void main(String [] args) throws IOException {
-
+    static int checkSecret;
+    static int []checkArr;
+    static int []myArr;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-
-        String str = br.readLine().toString();
-
+        char [] dna = new char[N];
+        dna = br.readLine().toCharArray();
         st = new StringTokenizer(br.readLine());
 
-        int [] checkArr = new int[4];
+        checkArr = new int[4];
+        myArr = new int[4];
+        checkSecret = 0;
 
-        for (int i = 0; i < 4; i++) {
-            checkArr[i] = Integer.parseInt(st.nextToken());
-        }
-        int cnt = 0;
-
-        for(int i=0; i<P; i++){
-            add(str.charAt(i));
-        }
-        if(isSuccess(arr, checkArr)){
-            cnt++;
-        }
-        for(int i=P; i<S; i++){
-            remove(str.charAt(i-P));
-            add(str.charAt(i));
-            if(isSuccess(arr, checkArr)){
-                cnt++;
+        for(int k=0; k<4; k++) {
+            checkArr[k] = Integer.parseInt(st.nextToken());
+            if(checkArr[k] == 0) {
+                checkSecret++;
             }
         }
-        System.out.println(cnt);
+
+        int result = 0;
+
+        for(int i=0; i<M; i++) {
+            Add(dna[i]);
+        }
+        if(checkSecret == 4) {
+            result ++;
+        }
+        for (int i=M; i<N; i++) {
+            int j = i-M;
+            Add(dna[i]);
+            Remove(dna[j]);
+            if(checkSecret == 4) {
+                result ++;
+            }
+        }
+        System.out.println(result);
+        br.close();
     }
-    private static void add(char c){
-        if(c =='A'){
-           arr[0] ++;
-        }
-        else if(c =='C'){
-            arr[1] ++;
-        }
-        else if(c =='G'){
-            arr[2] ++;
-        }
-        else if(c =='T'){
-            arr[3] ++;
+    private static void Add(char c) {
+        switch (c) {
+            case 'A':
+                myArr[0]++;
+                if(myArr[0] == checkArr[0]) {
+                    checkSecret++;
+                }
+                break;
+            case 'C':
+                myArr[1]++;
+                if(myArr[1] == checkArr[1]) {
+                    checkSecret++;
+                }
+                break;
+            case 'G':
+                myArr[2]++;
+                if(myArr[2] == checkArr[2]) {
+                    checkSecret++;
+                }
+                break;
+            case 'T':
+                myArr[3]++;
+                if(myArr[3] == checkArr[3]) {
+                    checkSecret++;
+                }
+                break;
         }
     }
-    private static void remove(char c){
-        if(c =='A'){
-            arr[0] --;
+    private static void Remove(char c) {
+        switch (c) {
+            case 'A':
+                if (myArr[0] == checkArr[0]) {
+                    checkSecret--;
+                }
+                myArr[0]--;
+                break;
+            case 'C':
+                if (myArr[1] == checkArr[1]) {
+                    checkSecret--;
+                }
+                myArr[1]--;
+                break;
+            case 'G':
+                if (myArr[2] == checkArr[2]) {
+                    checkSecret--;
+                }
+                myArr[2]--;
+                break;
+            case 'T':
+                if (myArr[3] == checkArr[3]) {
+                    checkSecret--;
+                }
+                myArr[3]--;
+                break;
         }
-        else if(c =='C'){
-            arr[1] --;
-        }
-        else if(c =='G'){
-            arr[2] --;
-        }
-        else if(c =='T'){
-            arr[3] --;
-        }
-    }
-    private static boolean isSuccess(int []arr, int[]checkArr){
-        if(checkArr[0]<=arr[0] && checkArr[1]<=arr[1] && checkArr[2]<=arr[2] && checkArr[3]<=arr[3]  ){
-            return true;
-        }
-        return false;
     }
 }
