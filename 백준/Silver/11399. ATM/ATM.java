d81e9ce0ccc521ed17ea1dcfe6f08ed0616930b5
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2025-01-02 22:04:23 +0900

    [Silver IV] Title: ATM, Time: 124 ms, Memory: 14996 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -8,16 +8,16 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
 
-        int []arr = new int[N];
-        int []result = new int[N];
+        int[] arr = new int[N];
+        int[] result = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 1; i < N; i++) {
+        for(int i = 1; i < N; i++) {
             int index = i;
             int num = arr[i];
-            for (int j = i-1; j >= 0; j--) {
-                if (arr[i] > arr[j]) {
+            for(int j = i-1; j >= 0; j--) {
+                if(arr[i] > arr[j]) {
                     index = j+1;
                     break;
                 }
@@ -31,11 +31,11 @@ public class Main {
             arr[index] = num;
         }
         result[0] = arr[0];
-        for (int i = 1; i < N; i++) {
+        for(int i = 1; i < N; i++) {
             result[i] = result[i-1] + arr[i];
         }
         int sum = 0;
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++) {
             sum += result[i];
         }
         System.out.println(sum);
