1e0f84f9920b05e00ef5560db43c3a1c77d100d3
SHIN YE RIN <nir2y@naver.com>
2025-01-06 14:07:43 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 15604 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,70 +1,51 @@
-import java.util.*;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[][] map;
-    static boolean[][] visit;
-    static int n;
-    static int m;
-    static int dx[] = {-1, 1, 0, 0};
-    static int dy[] = {0, 0, -1, 1};
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
-        m = sc.nextInt();
-        sc.nextLine();
-
-        map = new boolean[n][m];
-        visit = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            String str = sc.nextLine();
-            for (int j = 0; j < str.length(); j++) {
-                if (str.charAt(j) == '1') {
-                    map[i][j] = true;
-                }
+    static int N;
+    static int M;
+    static boolean[][] visited;
+    static int [][]A;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = new int[N][M];
+        visited = new boolean[N][M];
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            for(int j=0; j<M; j++) {
+                A[i][j] = Integer.parseInt(line.substring(j, j+1));
             }
         }
+        BFS(0, 0);
+        System.out.println(A[N-1][M-1]);
 
-        System.out.println(bfs(0, 0));
     }
-
-    private static int bfs(int x, int y) {
-        visit[x][y] = true;
-        Queue<Coordinate> q = new LinkedList<>();
-        q.add(new Coordinate(x, y, 1));
-
-        while (!q.isEmpty()) {
-            Coordinate c = q.remove();
-
-            if (c.x == n - 1 && c.y == m - 1)
-                return c.c;
-
-            for (int i = 0; i < 4; i++) {
-                int nowX = c.x + dx[i];
-                int nowY = c.y + dy[i];
-
-                if (nowX >= 0 && nowY >= 0 && nowX < n && nowY < m) {
-                    if (!visit[nowX][nowY] && map[nowX][nowY]) {
-                        visit[nowX][nowY] = true;
-                        q.add(new Coordinate(nowX, nowY, c.c + 1));
+    private static void BFS(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {i, j});
+        visited[i][j] = true;
+        while(!queue.isEmpty()) {
+            int cur[] = queue.poll();;
+            for(int k=0; k<4; k++) {
+                int x = cur[0] + dx[k];
+                int y = cur[1] + dy[k];
+                if(x >= 0 && y >= 0 && x < N && y < M ) {
+                    if(!visited[x][y] && A[x][y] != 0) {
+                        visited[x][y] = true;
+                        A[x][y] = A[cur[0]][cur[1]]+1;
+                        queue.add(new int[] {x, y});
                     }
                 }
             }
-        }
-        return 0;
-    }
 
-    static class Coordinate {
-        int x;
-        int y;
-        int c;
-
-        public Coordinate(int x, int y, int c) {
-            this.x = x;
-            this.y = y;
-            this.c = c;
         }
     }
 }
