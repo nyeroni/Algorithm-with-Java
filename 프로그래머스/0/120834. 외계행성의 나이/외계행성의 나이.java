097718b9c9e86ae2dd0eaaff08baab7f9fc04790
SHIN YE RIN <nir2y@naver.com>
2025-02-09 17:17:51 +0900

    [level 0] Title: 외계행성의 나이, Time: 12.97 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        String ageStr = String.valueOf(age);
+        for(int i=0; i<ageStr.length(); i++) {
+            answer += (char)(ageStr.charAt(i) - '0' + 'a');
+        }
+        return answer;
+    }
+}
