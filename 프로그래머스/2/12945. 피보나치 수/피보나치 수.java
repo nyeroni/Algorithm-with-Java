fcc1849ca3b037af408b3aa72fff0df89aa300fe
SHIN YE RIN <nir2y@naver.com>
2025-03-09 21:07:49 +0900

    [level 2] Title: 피보나치 수, Time: 3.49 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        return fibonacci(n);
+    }
+    public int fibonacci(int n) {
+        int []fibo = new int[n+1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for(int i=2; i<=n; i++) {
+            fibo[i] = (fibo[i-2] + fibo[i-1]) % 1234567; 
+        }
+        return fibo[n];
+    }
+}
