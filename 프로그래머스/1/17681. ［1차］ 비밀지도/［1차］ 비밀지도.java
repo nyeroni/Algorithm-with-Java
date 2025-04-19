e2b95496587cacc9e51fad2db40f06e9d2ada1aa
SHIN YE RIN <nir2y@naver.com>
2025-04-19 21:33:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.90 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for(int i=0; i<arr1.length; i++) {
+            String s1 = Integer.toString(arr1[i], 2);
+            String s2 = Integer.toString(arr2[i], 2);
+
+            StringBuffer sb1 = new StringBuffer(s1);
+            while(sb1.length() != n) {
+                sb1.insert(0, "0");
+            }
+            s1 = sb1.toString();
+            StringBuffer sb2 = new StringBuffer(s2);
+            while(sb2.length() != n) {
+                sb2.insert(0, "0");
+            }
+            s2 = sb2.toString();
+            String str = "";
+            for(int j=0; j<n; j++) {
+                if(s1.charAt(j) == '1' || s2.charAt(j) == '1') {
+                    str += "#";
+                } else {
+                    str += " ";
+                }
+            }
+            answer[i] = str;
+        }
+        return answer;
+    }
+}
