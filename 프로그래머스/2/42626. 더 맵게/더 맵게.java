a7b98f3a8bc5f8c459929920bf039d9f4488afac
SHIN YE RIN <nir2y@naver.com>
2025-04-23 21:12:51 +0900

    [level 2] Title: 더 맵게, Time: 1791.09 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -5,21 +5,19 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int s : scoville) {
-            pq.add(s);
+        for(int i=0; i<scoville.length; i++) {
+            pq.offer(scoville[i]);
         }
-        if(pq.peek() >= K) return 0;
-        while(pq.size()>1) {
-            int a = pq.poll();
-            int b = pq.poll();
-            int sum = a + b*2;
-            pq.add(sum);
+        
+        while(true) {
+            if(pq.peek() >= K) break;
+            if(pq.size() < 2) return -1;
+            int n = pq.poll();
+            int m = pq.poll();
+            int b = n + m * 2;
+            pq.offer(b);
             answer ++;
-            if(pq.peek() >= K) {
-                break;
-            }
         }
-        if(pq.size() == 1 && pq.peek() < K) return -1;
         return answer;
     }
 }
