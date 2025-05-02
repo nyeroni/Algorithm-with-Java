d33e364de984eff34a259bd1c1bd2451241812d8
SHIN YE RIN <nir2y@naver.com>
2025-05-02 16:39:41 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 22.01 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,9 +1,10 @@
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        long sqrtN = (long) Math.sqrt(n);
-        if(sqrtN*sqrtN == n) {
-            answer = (sqrtN + 1) * (sqrtN + 1);
+        long i = (long) Math.sqrt(n);
+        System.out.println("i : " + i);
+        if(i * i == n) {
+            answer = (i+1) * (i+1);
         }
         else answer = -1;
         return answer;
