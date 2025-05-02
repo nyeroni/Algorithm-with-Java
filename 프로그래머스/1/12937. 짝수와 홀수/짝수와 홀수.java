24426b7e92bebfd6d5738ee5f95673f78aef4cce
SHIN YE RIN <nir2y@naver.com>
2025-05-02 14:44:09 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(int num) {
-        String answer = "";
-        if(num%2==0) answer = "Even";
-        else answer = "Odd";
-        return answer;
+        if(num % 2 == 0) {
+            return "Even";
+        }
+        return "Odd";
     }
 }
