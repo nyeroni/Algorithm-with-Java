21d46a6dc3b5f0f38ad964c4e5db9504b263b766
SHIN YE RIN <nir2y@naver.com>
2025-05-02 16:49:57 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,12 +2,12 @@ import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
-        Arrays.sort(numbers);
-        boolean []tmp = new boolean[10];
+        boolean[] tmp = new boolean[10];
+        
         for(int i=0; i<numbers.length; i++) {
             tmp[numbers[i]] = true;
         }
-        for(int i=0; i<=9; i++) {
+        for(int i=1; i<=9; i++) {
             if(!tmp[i]) {
                 answer += i;
             }
