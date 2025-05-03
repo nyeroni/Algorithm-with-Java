ca088d8ee726145fadf6de2b6f1f6ade57603172
SHIN YE RIN <nir2y@naver.com>
2025-05-03 15:53:45 +0900

    [level 1] Title: 예산, Time: 0.53 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -2,15 +2,11 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int sum = 0;
         Arrays.sort(d);
         for(int i=0; i<d.length; i++) {
-            sum += d[i];
+            if(budget - d[i] < 0) break;
+            budget -= d[i];
             answer ++;
-            if(sum > budget) {
-                answer --;
-                break;
-            }
         }
         return answer;
     }
