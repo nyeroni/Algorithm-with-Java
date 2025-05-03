290fc6a0431e0fafe8f77707bdad08b61b11d736
SHIN YE RIN <nir2y@naver.com>
2025-05-03 13:43:43 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean solution(String s) {
-        String tmp = s;
-        if(s.length() != 4 && s.length() != 6) return false;
-        tmp = tmp.replaceAll("[^[0-9]]", "");
-        if(tmp.length() != s.length()) return false;
-        System.out.println("tmp : " + tmp);
-        return true;
+        int tmp = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                tmp ++;
+            }
+        }
+        if(tmp == s.length() && (s.length() == 4 || s.length() == 6)) return true;
+        return false;
     }
 }
