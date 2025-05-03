0a6c09c2b3fd4468da7806f6538cb617e5686914
SHIN YE RIN <nir2y@naver.com>
2025-05-03 16:49:34 +0900

    [level 2] Title: 숫자의 표현, Time: 0.46 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int start = 0;
         int sum = 0;
+        int start = 0;
         int answer = 0;
         for(int i=1; i<=n; i++) {
             sum += i;
             while(sum > n) {
                 sum -= start;
-                start ++;
+                start++;
             }
             if(sum == n) {
                 answer ++;
