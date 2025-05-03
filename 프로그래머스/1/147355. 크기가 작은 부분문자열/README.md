6417e138de00abb1a2eca39383c98da1c0adc279
SHIN YE RIN <nir2y@naver.com>
2025-05-03 16:23:10 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 8.53 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크기가 작은 부분문자열 - 147355 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355) 
 
 ### 성능 요약
 
-메모리: 91.8 MB, 시간: 11.04 ms
+메모리: 82 MB, 시간: 8.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 19일 18:56:22
+2025년 05월 03일 16:23:09
 
 ### 문제 설명
 
--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-        for(int i=0; i<=t.length() - p.length(); i++) {
-            String str = t.substring(i, i + p.length());
-            if(Long.parseLong(str) <= Long.parseLong(p)) {
-                answer ++;
-            }
+        for(int i=0; i<t.length() - p.length()+1; i++) {
+            String s = t.substring(i, i + p.length());
+            long a = Long.parseLong(s);
+            long b = Long.parseLong(p);
+            if(a <= b) answer ++;
         }
         return answer;
     }
