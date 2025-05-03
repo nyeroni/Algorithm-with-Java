cf8d28fd93f667841a3e61a0aab9a5a3f2185ba2
SHIN YE RIN <nir2y@naver.com>
2025-05-03 14:18:14 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.21 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -5,14 +5,12 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
-
         Arrays.sort(A);
         Arrays.sort(B);
-        
         for(int i=0; i<A.length; i++) {
-            answer += A[i] * B[B.length -1 -i];
+            answer += A[i] * B[B.length-1-i];
         }
-
+        
         return answer;
     }
 }
