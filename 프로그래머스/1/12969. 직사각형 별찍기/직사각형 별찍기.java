71087af105aedcf357564d4369fa6ca48d75d690
SHIN YE RIN <nir2y@naver.com>
2025-05-03 14:04:25 +0900

    [level 1] Title: 직사각형 별찍기, Time: 240.64 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        for(int i=0; i<b; i++) {
-            for(int j=0; j<a; j++) {
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
