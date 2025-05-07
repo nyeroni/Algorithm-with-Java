8a060399f0a20cca32c5c42675dd5bbf3d7ece05
SHIN YE RIN <nir2y@naver.com>
2025-05-07 17:20:19 +0900

    [level 1] Title: K번째수, Time: 2.77 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int [] ans = new int[commands.length];
+        int[] ans = new int[commands.length];
+        
         for(int i=0; i<commands.length; i++) {
-            int a = commands[i][0];
-            int b = commands[i][1];
-            int []tmp = new int[b - a + 1];
+            int []tmp = new int[commands[i][1] - commands[i][0] + 1];
             int k=0;
-            for(int j=a-1; j<b; j++) {
+            for(int j=commands[i][0]-1; j< commands[i][1]; j++) {
                 tmp[k++] = array[j];
             }
             Arrays.sort(tmp);
