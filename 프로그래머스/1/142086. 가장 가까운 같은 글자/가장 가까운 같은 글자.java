ad6cdc2fc26df7e821db490f38d29a3c4a34b97f
SHIN YE RIN <nir2y@naver.com>
2025-05-07 13:11:09 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 9.10 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
-    
+        boolean flag = false;
         for(int i=0; i<s.length(); i++) {
-            int num = -1;
-            for(int j=i-1; j>=0; j--) {
+            flag = false;
+            for(int j=i-1; j>=0; j--){
                 if(s.charAt(i) == s.charAt(j)) {
-                    num = i-j;
+                    answer[i] = i-j;
+                    flag = true;
                     break;
                 }
             }
-            answer[i] = num;
+            if(!flag) {
+                answer[i] = -1;
+            }
         }
         return answer;
     }
