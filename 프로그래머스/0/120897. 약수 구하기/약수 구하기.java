bf73562c888da0f6da6aa493c591553771b8cb38
SHIN YE RIN <nir2y@naver.com>
2025-05-12 22:12:28 +0900

    [level 0] Title: 약수 구하기, Time: 3.17 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer;
+        List<Integer> tmp = new ArrayList<>();
+        for(int i=1; i<=n; i++) {
+            if(n % i == 0) {
+                tmp.add(i);
+            }
+        }
+        answer = tmp.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+}
