9e490c314e07d33030666263be20e36387b97dc9
SHIN YE RIN <nir2y@naver.com>
2025-05-23 13:06:44 +0900

    [level 3] Title: 단어 변환, Time: 0.13 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,17 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
         int cnt = 0;
+        Queue<String> queue = new LinkedList<>();
+        boolean[] visited = new boolean[words.length];
         boolean flag = false;
-        for(String s : words) {
-            if(s.equals(target)) {
+        for(int i=0; i<words.length; i++) {
+            if(words[i].equals(target)) {
                 flag = true;
             }
         }
         if(!flag) return 0;
-        boolean[] visited = new boolean[words.length];
-        Queue<String> queue = new LinkedList<>();
+        
         queue.offer(begin);
         flag = false;
+        
         while(!queue.isEmpty()) {
             String now = queue.poll();
             if(check(now, target)) {
@@ -24,9 +26,9 @@ class Solution {
             for(int i=0; i<words.length; i++) {
                 if(!visited[i]) {
                     if(check(now, words[i])) {
-                        queue.offer(words[i]);
                         visited[i] = true;
                         cnt ++;
+                        queue.offer(words[i]);
                         break;
                     }
                 }
@@ -38,9 +40,13 @@ class Solution {
     public boolean check(String str1, String str2) {
         int cnt = 0;
         for(int i=0; i<str1.length(); i++) {
-            if(str1.charAt(i) == str2.charAt(i)) cnt ++;
+            if(str1.charAt(i) == str2.charAt(i)) {
+                cnt ++;
+            }
         }
-        if(cnt == str1.length()-1) return true;
+        if(cnt == str1.length()-1) {
+            return true;
+        }
         return false;
     }
 }
