9a523828378be21814a383027d96440eeeb7b7fe
SHIN YE RIN <nir2y@naver.com>
2025-05-23 09:41:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.96 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        int []di = {-1, 1, 0, 0};
-        int []dj = {0, 0, -1, 1};
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
         
         int row = maps.length;
         int col = maps[0].length;
@@ -11,25 +11,26 @@ class Solution {
         Queue<int[]> queue = new LinkedList<>();
         boolean[][] visited = new boolean[row][col];
         
-        queue.offer(new int[]{0, 0, 1});
+        queue.offer(new int[] {0, 0, 1});
         visited[0][0] = true;
         
         while(!queue.isEmpty()) {
-            int []current = queue.poll();
-            int i = current[0];
-            int j = current[1];
-            int dist = current[2];
+            int[] tmp = queue.poll();
+            int x = tmp[0];
+            int y = tmp[1];
+            int dist = tmp[2];
             
-            if(i == row-1 && j == col-1) {
+            if(x == row-1 && y == col-1) {
                 return dist;
             }
-            for(int k=0; k<4; k++) {
-                int ni = di[k] + i;
-                int nj = dj[k] + j;
-                if(ni >= 0 && ni < row && nj >= 0 && nj < col) {
-                    if(!visited[ni][nj] && maps[ni][nj] == 1) {
-                        visited[ni][nj] = true;
-                        queue.offer(new int[]{ni, nj, dist + 1});
+            
+            for(int i=0; i<4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= 0 && nx < row && ny >= 0 && ny < col) {
+                    if(!visited[nx][ny] && maps[nx][ny] == 1) {
+                        visited[nx][ny] = true;
+                        queue.offer(new int[]{nx, ny, dist + 1});
                     }
                 }
             }
