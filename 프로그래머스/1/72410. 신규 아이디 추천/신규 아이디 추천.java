54b0445b7a4eca2c7d603e1b223bd2aed2f9b495
SHIN YE RIN <nir2y@naver.com>
2025-05-23 14:23:20 +0900

    [level 1] Title: 신규 아이디 추천, Time: 3.67 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -3,10 +3,11 @@ class Solution {
         new_id = new_id.toLowerCase();
         new_id = new_id.replaceAll("[^[a-z0-9-_.]]", "");
         new_id = new_id.replaceAll("[.]{2,}", ".");
-        new_id = new_id.replaceAll("^[.]", "");
+        new_id = new_id.replaceAll("^[.]","");
         new_id = new_id.replaceAll("^\\s$", "a");
         new_id = new_id.replaceAll("^(.{15}).*", "$1");
         new_id = new_id.replaceAll("[.]$", "");
+        
         if(new_id.length() < 1) {
             return "aaa";
         }
