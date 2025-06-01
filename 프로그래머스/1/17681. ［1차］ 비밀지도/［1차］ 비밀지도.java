1b44a65ece0ff378208c7fff7071db0cbb5ecc5f
SHIN YE RIN <nir2y@naver.com>
2025-06-01 17:00:54 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.41 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -4,17 +4,21 @@ class Solution {
         for(int i=0; i<arr1.length; i++) {
             String s1 = Integer.toString(arr1[i], 2);
             String s2 = Integer.toString(arr2[i], 2);
-
+            
             StringBuffer sb1 = new StringBuffer(s1);
+            StringBuffer sb2 = new StringBuffer(s2);
+            
             while(sb1.length() != n) {
                 sb1.insert(0, "0");
             }
-            s1 = sb1.toString();
-            StringBuffer sb2 = new StringBuffer(s2);
+            
             while(sb2.length() != n) {
                 sb2.insert(0, "0");
             }
+            
+            s1 = sb1.toString();
             s2 = sb2.toString();
+            
             String str = "";
             for(int j=0; j<n; j++) {
                 if(s1.charAt(j) == '1' || s2.charAt(j) == '1') {
