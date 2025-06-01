e9393172411419f61fa19b63749462afcb7d273b
SHIN YE RIN <nir2y@naver.com>
2025-06-01 17:35:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 56.75 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,44 +3,39 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        for(int i=0; i<s.length(); i++) {
-            s += s.charAt(0);
-            s = s.substring(1, s.length());
-            if(check(s)) {
-                answer ++;
-            }
-        }
+       for(int i=0; i<s.length(); i++) {
+           s += s.charAt(0);
+           s = s.substring(1, s.length());
+           if(check(s)) answer ++;
+       }
         return answer;
     }
-    public boolean check(String s) {
+    private boolean check(String s) {
         Stack<String> stack = new Stack<>();
         for(int i=0; i<s.length(); i++) {
             if(s.charAt(i) == '(') {
                 stack.push("(");
-            } else if(s.charAt(i) == '{') {
-                stack.push("{");
             } else if(s.charAt(i) == '[') {
                 stack.push("[");
-            }
-            else {
-                if(stack.isEmpty()) {
-                    return false;
-                }
+
+            } else if(s.charAt(i) == '{') {
+                stack.push("{");
+
+            } else {
+                if(stack.isEmpty()) return false;
                 String str = stack.peek();
                 if(str.equals("(") && s.charAt(i) == ')') {
                     stack.pop();
-                } 
-                else if(str.equals("{") && s.charAt(i) == '}') {
+                } else if(str.equals("{") && s.charAt(i) == '}') {
                     stack.pop();
-                }
-                else if(str.equals("[") && s.charAt(i) == ']') {
+                } else if(str.equals("[") && s.charAt(i) == ']') {
                     stack.pop();
                 }
             }
         }
-        if(!stack.isEmpty()) {
-            return false;
-        }
-        return true;
+        if(stack.isEmpty()) {
+            return true;
+        } 
+        return false;
     }
 }
