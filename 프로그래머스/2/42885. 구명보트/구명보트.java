f2db480a4906534175149866a9aafdce58c050b1
SHIN YE RIN <nir2y@naver.com>
2025-06-01 16:14:08 +0900

    [level 2] Title: 구명보트, Time: 10.59 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,11 +3,10 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int index = 0;
-        for(int i=people.length-1; i>=index; i--) {
-        
-            if(people[i] + people[index] <= limit) {
-                index ++;
+        int m = 0;
+        for(int i=people.length-1; i>=m; i--) {
+            if(people[i] + people[m] <= limit) {
+                m++;
             }
             answer ++;
         }
