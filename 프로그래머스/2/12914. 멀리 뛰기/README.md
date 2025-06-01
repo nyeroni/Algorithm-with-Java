f292f4a03eb6361747addfb9d00a10cf7389ecb6
SHIN YE RIN <nir2y@naver.com>
2025-06-01 16:32:03 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
 
 ### 성능 요약
 
-메모리: 86.3 MB, 시간: 0.20 ms
+메모리: 77.3 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 15:27:11
+2025년 06월 01일 16:32:01
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,14 @@
 class Solution {
     public long solution(int n) {
-        long[] dp = new long[n + 2];
+        long []dp = new long[n+1];
+        if(n==1) return 1;
+        else if(n==2) return 2;
         dp[0] = 1;
         dp[1] = 1;
-        int mod = 1234567;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+        
+        for(int i=2; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
-
         return dp[n];
     }
 }
