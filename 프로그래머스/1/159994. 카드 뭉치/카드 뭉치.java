4dee1cc125adde98ce759949d68bf6751e625f6f
SHIN YE RIN <nir2y@naver.com>
2025-06-01 16:29:24 +0900

    [level 1] Title: 카드 뭉치, Time: 0.12 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,14 +1,26 @@
+import java.util.*;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        int i=0, j=0;
-        String answer = "";
-        for(int k=0; k<goal.length; k++) {
-            if(i < cards1.length && goal[k].equals(cards1[i])) {
-                i++;
-            } else if(j < cards2.length && goal[k].equals(cards2[j])) {
-                j++;
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+        
+        for(int i=0; i<cards1.length; i++) {
+            queue1.offer(cards1[i]);
+        }
+        for(int i=0; i<cards2.length; i++) {
+            queue2.offer(cards2[i]);
+        }
+        
+        for(int i=0; i<goal.length; i++) {
+            String str1 = queue1.peek();
+            String str2 = queue2.peek();
+            
+            if(goal[i].equals(str1)) {
+                queue1.poll();
             }
-            else {
+            else if(goal[i].equals(str2)) {
+                queue2.poll();
+            } else {
                 return "No";
             }
         }
