677ae3f8d3bd88a82ca79a6984c274e34555f02e
SHIN YE RIN <nir2y@naver.com>
2025-06-01 17:07:26 +0900

    [level 1] Title: 추억 점수, Time: 2.49 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
         Map<String, Integer> map = new HashMap<>();
+        
         for(int i=0; i<name.length; i++) {
             map.put(name[i], yearning[i]);
         }
-        int[] answer = new int[photo.length];
         for(int i=0; i<photo.length; i++) {
-            int sum = 0;
+            int count = 0;
             for(int j=0; j<photo[i].length; j++) {
                 if(map.containsKey(photo[i][j])) {
-                    sum += map.get(photo[i][j]);
+                    count += map.get(photo[i][j]);
                 }
             }
-            answer[i] = sum;
+            answer[i] = count;
         }
         return answer;
     }
