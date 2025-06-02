c759410212e197ec050facb160ac5a8317dac28d
SHIN YE RIN <nir2y@naver.com>
2025-06-02 17:42:07 +0900

    [level 2] Title: H-Index, Time: 17.72 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,11 +1,8 @@
 import java.util.*;
 class Solution {
     public int solution(int[] citations) {
-        int answer = -1;
+        int cnt=0, h=0, answer = -1;
         Arrays.sort(citations);
-        //0 1 3 5 6
-        int h = 0;
-        int cnt = 0;
         while(true) {
             cnt = 0;
             for(int i=0; i<citations.length; i++) {
@@ -14,12 +11,11 @@ class Solution {
                 }
             }
             if(cnt < h) {
-                answer = h-1;
+                answer =  h -1;
                 break;
             }
             h++;
         }
-        
         return answer;
     }
 }
