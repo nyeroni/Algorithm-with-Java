23e9bbc2ace8cdc91314f2512e643adbc79603d5
SHIN YE RIN <nir2y@naver.com>
2025-06-24 17:26:55 +0900

    [level 2] Title: 피로도, Time: 4.04 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -3,14 +3,14 @@ import java.util.*;
 class Solution {
     int max = 0;
     public int solution(int k, int[][] dungeons) {
-        boolean []visited = new boolean[dungeons.length];
+        boolean[]visited = new boolean[dungeons.length];
         dfs(k, dungeons, visited, 0);
         return max;
     }
     public void dfs(int k, int [][]dungeons, boolean[] visited, int depth) {
         max = Math.max(depth, max);
         for(int i=0; i<dungeons.length; i++) {
-            if(!visited[i] && dungeons[i][0] <= k) {
+            if(!visited[i] && k >= dungeons[i][0]) {
                 visited[i] = true;
                 dfs(k - dungeons[i][1], dungeons, visited, depth+1);
                 visited[i] = false;
