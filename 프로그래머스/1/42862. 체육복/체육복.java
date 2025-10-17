6b12d9ff49b8930382c562e19582590914a7d173
SHIN YE RIN <nir2y@naver.com>
2025-10-17 23:42:05 +0900

    [level 1] Title: 체육복, Time: 0.56 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,51 +1,41 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
         Arrays.sort(lost);
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> lostTmp = new ArrayList<>();
-        List<Integer> reserveTmp = new ArrayList<>();
-        for(int i=0; i<lost.length; i++) {
-            lostTmp.add(lost[i]);
-        }
-        for(int i=0; i<reserve.length; i++) {
-            reserveTmp.add(reserve[i]);
-        }
-        for(int i=1; i<=n; i++) {
-            if(reserveTmp.contains(i)) {
-                map.put(i, map.getOrDefault(i, 0) + 1);
-            } 
-            if(lostTmp.contains(i)) {
-                map.put(i, map.getOrDefault(i, 0) - 1);
-            }
-            map.put(i, map.getOrDefault(i, 0));
-        }
-
-        for(int i=0; i<lost.length; i++) {
-            if(map.get(lost[i]) == 0) {
-                continue;
-            }
-            int l1 = lost[i] - 1;
-            int l2 = lost[i] + 1;
-            if(l1 > 0 && map.get(l1) == 1) {
-                map.put(l1, map.get(l1) - 1);
-                map.put(lost[i], map.get(lost[i]) + 1);
-                continue;
-            }
-            else if(l2 <= n && map.get(l2) == 1) {
-                map.put(l2, map.get(l2) - 1);
-                map.put(lost[i], map.get(lost[i]) + 1);
-            }
-
-        }
-    
-        for(int i : map.keySet()) {
-            // System.out.println("i  : " + i + " map.get(i) : " + map.get(i));
-            if(map.get(i) >= 0) {
-                answer ++;
+        Arrays.sort(reserve);
+        
+        List<Integer> lostList = new ArrayList<>();
+        List<Integer> reserveList = new ArrayList<>();
+        
+        for(int l : lost) {
+            if(!contains(reserve, l)) {
+                lostList.add(l);
             }
         }
-        return answer;
+        for(int r : reserve) {
+            if(!contains(lost, r)) {
+                reserveList.add(r);
+            }
+        }
+        
+        int attend = n - lostList.size();
+        
+        for(int l : lostList) {
+            if(reserveList.contains(l-1)) {
+                reserveList.remove(Integer.valueOf(l-1));
+                attend ++;
+            } else if(reserveList.contains(l+1)) {
+                reserveList.remove(Integer.valueOf(l+1));
+                attend ++;
+            }
+        }
+        return attend;
+    }
+    private boolean contains(int[] arr, int value) {
+        for(int i : arr) {
+            if(i==value) return true;
+        }
+        return false;
     }
 }
