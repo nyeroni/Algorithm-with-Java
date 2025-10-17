d8d22112caabe89a7f8ae3c3762b5a93feb52cf7
SHIN YE RIN <nir2y@naver.com>
2025-10-17 19:05:59 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,12 +1,12 @@
 class Solution {
     public long solution(int n) {
-        long []dp = new long[n+1];
+        long[] dp = new long[n+1];
         if(n==1) return 1;
         else if(n==2) return 2;
-        dp[0] = 1;
         dp[1] = 1;
-        
-        for(int i=2; i<=n; i++) {
+        dp[2] = 2;
+    
+        for(int i=3; i<=n; i++) {
             dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
         return dp[n];
