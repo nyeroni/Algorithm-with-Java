29481e75a2262cf54629242ca71c6e88dbd531c3
SHIN YE RIN <nir2y@naver.com>
2025-10-17 19:06:13 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.29 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,26 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-        answer[0] = 0;
-        answer[1] = 0;
+        int []answer = new int[2];
+        List<String> list = new LinkedList<>();
         int count = 1;
-        char c = words[0].charAt(words[0].length()-1);
-        if(words[0].length() == 1) return new int[]{1, 1};
-        List<String> tmp = new ArrayList<>();
-        tmp.add(words[0]);
+        list.add(words[0]);
         for(int i=1; i<words.length; i++) {
-            if((i+1) % n == 1) {
-                count ++;
-            }
-            if(words[i].charAt(0) != c || tmp.contains(words[i]) || words[i].length() == 1) {
+            if(i%n == 0) count ++;
+            if(words[i-1].charAt((words[i-1].length()-1)) != words[i].charAt(0) || list.contains(words[i])) {
                 answer[0] = i%n + 1;
                 answer[1] = count;
-                break;
+                return answer;
             }
-            c = words[i].charAt(words[i].length()-1);
-            tmp.add(words[i]);
+            list.add(words[i]);
         }
+    
         return answer;
     }
 }
