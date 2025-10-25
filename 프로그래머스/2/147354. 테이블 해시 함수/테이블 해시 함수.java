ec0ab5e551bafafe0a51f95d1538ebc4aac55897
SHIN YE RIN <nir2y@naver.com>
2025-10-25 21:25:09 +0900

    [level 2] Title: 테이블 해시 함수, Time: 25.62 ms, Memory: 160 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        
+        Arrays.sort(data, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if(a[col - 1] == b[col - 1]) {
+                    return b[0] - a[0];
+                } else {
+                    return a[col-1] - b[col-1];
+                }
+            }
+        });
+        for(int i=row_begin-1; i<row_end; i++) {
+            int sum = 0;
+            for(int j=0; j<data[i].length; j++) {
+                sum += (data[i][j]  % (i+1));
+            }
+            answer ^= sum;
+        }
+        return answer;
+    }
+}
