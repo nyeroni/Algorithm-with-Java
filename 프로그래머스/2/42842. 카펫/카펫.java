37397481d979b11bcea8a795c3892e2b4fba6b26
SHIN YE RIN <nir2y@naver.com>
2025-10-30 00:35:47 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,21 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = countBrown(brown, yellow);
-        return answer;
+        return count(brown, yellow);
     }
-    private int[] countBrown(int brown, int yellow) {
-        int x = 0, y=0;
-        int count = 0;
-        while(count != brown) {
-            x ++;
-            if(yellow%x == 0) {
-                y = yellow/x;
-            } else continue;
-            System.out.println("X : " + x);
-            System.out.println("Y : " + y);
-            count = x * 2 + (y+2) * 2;
-            if(count == brown) {
-                return new int[] {y+2, x+2} ;
+    private int[] count(int brown, int yellow) {
+        // brown = 2*x + 2*y - 4
+        // yellow = (x-2) * (y-2)
+        //brow + yellow = x * y;
+        int sum = brown + yellow;
+        for(int i=1; i<=Math.sqrt(sum); i++) {
+            if(sum % i == 0) {
+                int x = i;
+                int y = sum / x;
+                if( 2 * x + 2 * y - 4 == brown && (x-2) * (y-2) == yellow) {
+                    return new int[]{y, x};
+                }
             }
         }
-        return new int[] {0, 0};
+        return new int[]{0, 0};
     }
 }
