f3768550391384869c495c6ff2c2008e9695eeb6
SHIN YE RIN <nir2y@naver.com>
2025-10-30 01:48:00 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 187.70 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -2,28 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
         Set<Integer> set = new HashSet<>();
-        int len = 1;
-        while(true) {
-            int sum = 0;
-            for(int i=0; i<elements.length; i++) {
-                sum = 0;
-                for(int j=i; j<len + i; j++) {
-                    int k =j;
-                    if(j >= elements.length) {
-                        k = j % elements.length;
-                    }
-                    sum += elements[k];
-                }
+        for(int i=0; i<elements.length; i++) {
+            int sum = elements[i];
+            int j = i+1;
+            while(j != i) {
                 set.add(sum);
+                if(j==elements.length) {
+                    j = 0;
+                }
+                if(j == i) break;
+                sum += elements[j];
+                // System.out.println("sum : " + sum);
+                set.add(sum);
+                j++;
             }
-            if(len == elements.length) {
-                break;
-            }
-            len++;
         }
-        answer = set.size();
-        return answer;
+        
+        return set.size();
     }
 }
