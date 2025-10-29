7db9fffa6617ebb050dec8ec79a06ef32900d4ab
SHIN YE RIN <nir2y@naver.com>
2025-10-30 02:15:27 +0900

    [level 2] Title: 괄호 회전하기, Time: 46.01 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,39 +3,42 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-       for(int i=0; i<s.length(); i++) {
-           s += s.charAt(0);
-           s = s.substring(1, s.length());
-           if(check(s)) answer ++;
-       }
+        // if(calculate(s)) answer ++;
+        int cnt = 0;
+        while(cnt < s.length()) {
+            // System.out.println(" s : " + s);
+            // System.out.println("calculate(s) :" + calculate(s));
+            if(calculate(s)) answer ++;
+            char c = s.charAt(0);
+            s = s.substring(1, s.length());
+            s += c;
+            cnt ++;
+        }
         return answer;
     }
-    private boolean check(String s) {
-        Stack<String> stack = new Stack<>();
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '(') {
-                stack.push("(");
-            } else if(s.charAt(i) == '[') {
-                stack.push("[");
-
-            } else if(s.charAt(i) == '{') {
-                stack.push("{");
-
-            } else {
-                if(stack.isEmpty()) return false;
-                String str = stack.peek();
-                if(str.equals("(") && s.charAt(i) == ')') {
+    private boolean calculate(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c == '(') stack.push('(' );
+            else if (c == ')') {
+                if(!stack.isEmpty() && stack.peek() == '(') {
                     stack.pop();
-                } else if(str.equals("{") && s.charAt(i) == '}') {
+                } else return false;
+            } 
+            if(c == '[') stack.push('[' );
+            else if (c == ']') {
+                if(!stack.isEmpty() && stack.peek() == '[') {
                     stack.pop();
-                } else if(str.equals("[") && s.charAt(i) == ']') {
+                } else return false;
+            } 
+            if(c == '{') stack.push('{' );
+            else if (c == '}') {
+                if(!stack.isEmpty() && stack.peek() == '{') {
                     stack.pop();
-                }
-            }
+                } else return false;
+            } 
         }
-        if(stack.isEmpty()) {
-            return true;
-        } 
+        if(stack.isEmpty()) return true;
         return false;
     }
 }
