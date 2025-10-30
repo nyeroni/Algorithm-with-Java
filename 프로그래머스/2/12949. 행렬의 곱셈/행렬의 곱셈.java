e49515c0cf5d175cb93a61e197f9bec07b0f5c67
SHIN YE RIN <nir2y@naver.com>
2025-10-30 22:04:34 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.33 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -4,23 +4,20 @@ class Solution {
         for(int i=0; i<arr1.length; i++) {
             for(int j=0; j<arr2[0].length; j++) {
                 int sum = 0;
-                for(int k=0; k<arr1[i].length; k++) {
+                for(int k=0; k<arr1[0].length; k++) {
                     sum += arr1[i][k] * arr2[k][j];
                 }
                 answer[i][j] = sum;
             }
         }
-        
         return answer;
+        // 1 4    3 3  00*00 + 01*10     00*01 + 01*11
+        // 3 2    3 3  10*00 + 10*10     
+        // 4 1    
+        
+        // 15 15
+        // 15 15
+        // 15 15
+        
     }
-    /*
-    1 4
-    3 2 
-    4 1
-    
-    3 3
-    3 3
-    
-    00 * 00 + 01 * 10, 00 * 01 + 01 * 11
-    */
 }
