c91a489202c7d61600f3f16a2791f206a9a294f8
SHIN YE RIN <nir2y@naver.com>
2025-10-30 23:53:45 +0900

    [level 2] Title: 프로세스, Time: 1.90 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 75.5 MB, 시간: 1.85 ms
+메모리: 94.3 MB, 시간: 1.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 01일 11:22:57
+2025년 10월 30일 23:53:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,34 +1,38 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
         Queue<int[]> queue = new LinkedList<>();
         for(int i=0; i<priorities.length; i++) {
             queue.offer(new int[]{i, priorities[i]});
-        }
+        } 
         boolean []visited = new boolean[priorities.length];
+        int[] tmp = new int[priorities.length];
+        int k=0;
         while(!queue.isEmpty()) {
-            int []now = queue.poll();
+            int[] now = queue.poll();
             int idx = now[0];
             int num = now[1];
             visited[idx] = true;
             boolean flag = false;
             for(int i=0; i<priorities.length; i++) {
-                if(visited[i]) continue;
-                if(num < priorities[i]) {
+                if(!visited[i] && num < priorities[i]) {
                     flag = true;
                 }
             }
             if(flag) {
-                queue.offer(new int[] {idx, num});
-                visited[idx] = false;
+                queue.offer(new int[]{idx, num});
+                visited[idx] = false; 
             } else {
-                answer ++;
-                if(location == idx) {
-                    break;
-                }
+                tmp[k] = idx;
+                k++;
             }
-        
+        }
+        int answer =0;
+        for(int i=0; i<tmp.length; i++) {
+            // System.out.println("tmp[i] : " +tmp[i]);
+            if(tmp[i] == location) {
+                answer = i+1;
+            }
         }
         return answer;
     }
