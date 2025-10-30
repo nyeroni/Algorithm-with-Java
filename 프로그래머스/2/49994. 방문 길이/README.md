00e6002fa024c8d96fb5dcb8469388069a210cd8
SHIN YE RIN <nir2y@naver.com>
2025-10-31 01:12:00 +0900

    [level 2] Title: 방문 길이, Time: 20.52 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
 
 ### 성능 요약
 
-메모리: 85.9 MB, 시간: 27.76 ms
+메모리: 84.9 MB, 시간: 20.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 10일 20:33:14
+2025년 10월 31일 01:11:58
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,74 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(String dirs) {
-        int answer = 0;
-        Set<String> set = new HashSet<String>(); 
-                
+        int target = dirs.length();
         int x = 0;
         int y = 0;
-        
-        for(int i=0; i<dirs.length(); i++){
+        Set<String> set = new HashSet<>();
+        for(char c : dirs.toCharArray()) {
             int nx = x;
             int ny = y;
-            String pos = ""; 
-            String tmp = "";
-            if(dirs.charAt(i) == 'U'){
-                ny++;
-                pos += x;
-                pos += y;
-                pos += nx;
-                pos += ny;
-                tmp += nx;
-                tmp += ny;
-                tmp += x;
-                tmp += y;
-
-            }
-            else if(dirs.charAt(i) == 'D'){
-                ny--;
-                pos += x;
-                pos += y;
-                pos += nx;
-                pos += ny;
-                tmp += nx;
-                tmp += ny;
-                tmp += x;
-                tmp += y;
-            }
-            else if(dirs.charAt(i) == 'R'){
-                nx++;
-                pos += x;
-                pos += y;
-                pos += nx;
-                pos += ny;
-                tmp += nx;
-                tmp += ny;
-                tmp += x;
-                tmp += y;
-            }
-            else if(dirs.charAt(i) == 'L'){
-                nx--;
-                pos += x;
-                pos += y;
-                pos += nx;
-                pos += ny;
-                tmp += nx;
-                tmp += ny;
-                tmp += x;
-                tmp += y;
-            }
-            if(nx < -5 || ny < -5 || nx > 5 || ny > 5) {
-                continue;
-            }       
-       
-            if(!set.contains(tmp) && !set.contains(pos)) {
-                set.add(pos);
-            }
+            if(c=='U') ny++;
+            else if(c=='D') ny--;
+            else if(c=='L') nx--;
+            else if(c=='R') nx++;
+            if(ny > 5 || nx > 5 || nx < -5 || ny < -5) continue;
+            
+            String path1 = x+","+y+","+nx+","+ny;
+            String path2 = nx+","+ny+","+x+","+y;
+            
+            set.add(path1);
+            set.add(path2);
             
             x = nx;
             y = ny;
         }
-        answer = set.size();
-        return answer;
+        return set.size()/2;
     }
 }
