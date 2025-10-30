b7ce1d053f1328a05f60b6144bb79be80ad889c4
SHIN YE RIN <nir2y@naver.com>
2025-10-31 00:06:01 +0900

    [level 2] Title: 롤케이크 자르기, Time: 217.04 ms, Memory: 192 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,26 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
+        Map<Integer, Integer> mapA = new HashMap<>();
+        Map<Integer, Integer> mapB = new HashMap<>();
         int answer = 0;
-        Map<Integer, Integer> map1 = new HashMap<>();
-        Map<Integer, Integer> map2 = new HashMap<>();
-
         for(int i=0; i<topping.length; i++) {
-            int num = topping[i];
-            map2.put(num, map2.getOrDefault(num, 0) + 1);
+           mapB.put(topping[i], mapB.getOrDefault(topping[i], 0) + 1);
         }
-        
         for(int i=0; i<topping.length; i++) {
-            int num = topping[i];
-            map1.put(num, map1.getOrDefault(num, 0) + 1);
-            if(map2.containsKey(num)) {
-                if(map2.get(num) == 1) {
-                    map2.remove(num);
-                } else {
-                    map2.put(num, map2.getOrDefault(num, 0)-1);
-                }
-            }
-            if(map2.size() == map1.size()) {
+            mapA.put(topping[i], mapA.getOrDefault(topping[i], 0) + 1);
+            if(mapB.get(topping[i]) == 1) {
+                mapB.remove(topping[i]);
+            } else  mapB.put(topping[i], mapB.getOrDefault(topping[i], 0) - 1);
+            if(mapA.size() == mapB.size()) {
                 answer ++;
             }
         }
