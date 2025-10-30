3109dd482cb06d1b090f5de7845c184978eea732
SHIN YE RIN <nir2y@naver.com>
2025-10-30 23:35:52 +0900

    [level 2] Title: 튜플, Time: 89.33 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        int[] answer;
+        
         s = s.substring(2, s.length()-2);
         String[] str = s.split("\\},\\{");
         Arrays.sort(str, new Comparator<String>() {
-            @Override
             public int compare(String a, String b) {
-                return a.length() - b.length();
+                return a.length()-b.length();
             }
-            
         });
-        List<Integer> tuple = new ArrayList<>();
-        for(String ss : str) {
-            String[] tmp = ss.split(",");
+        List<Integer> list = new ArrayList<>();
+        for(String st : str) {
+            String[] tmp = st.split(",");
             for(String t : tmp) {
                 int num = Integer.parseInt(t);
-                if(tuple.contains(num)) continue;
-                tuple.add(num);
+                if(list.contains(num)) continue;
+                list.add(num);
             }
         }
-        answer = tuple.stream().mapToInt(i->i).toArray();
-        return answer;
+        
+        return list.stream().mapToInt(i->i).toArray();
     }
 }
