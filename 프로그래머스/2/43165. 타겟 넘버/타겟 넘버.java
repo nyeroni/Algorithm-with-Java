3ee99a63e32d8f2e63a03b3e3baf1ea715401737
SHIN YE RIN <nir2y@naver.com>
2025-11-01 02:16:19 +0900

    [level 2] Title: 타겟 넘버, Time: 5.77 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,16 @@
 class Solution {
-    static int cnt = 0;
+    int cnt = 0;
     public int solution(int[] numbers, int target) {
         boolean[] visited = new boolean[numbers.length];
-        dfs(numbers, 0, target, 0);
+        dfs(numbers, target, 0, 0);
         return cnt;
     }
-    private void dfs(int[] numbers, int num, int target, int depth) {
+    private void dfs(int[] numbers, int target, int sum, int depth) {
         if(depth == numbers.length) {
-            if(num == target) cnt ++;
+            if(target == sum) cnt++;
             return;
         }
-        dfs(numbers, num + numbers[depth], target, depth + 1);
-        dfs(numbers, num - numbers[depth], target, depth + 1);
+        dfs(numbers, target, sum + numbers[depth], depth + 1);
+        dfs(numbers, target, sum - numbers[depth], depth + 1);
     }
 }
