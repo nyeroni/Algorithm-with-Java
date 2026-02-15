5a59ee1a7afcd83a06c62da2995a8a6e997d401a
SHIN YE RIN <nir2y@naver.com>
2026-02-15 21:59:02 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,11 +1,9 @@
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
-        int sum = 0;
-        for(int i=0; i<arr.length; i++){
-            sum += arr[i];
+        double sum = 0.0;
+        for(int i : arr) {
+            sum += i;
         }
-        answer = (double)sum / (double)arr.length;
-        return answer;
+        return sum / (double) arr.length;
     }
 }
