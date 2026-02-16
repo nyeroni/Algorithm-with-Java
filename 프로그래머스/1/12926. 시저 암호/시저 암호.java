bd37290fda11d135f69408cf3e36212172372aaa
SHIN YE RIN <nir2y@naver.com>
2026-02-16 18:07:01 +0900

    [level 1] Title: 시저 암호, Time: 1.40 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,17 +1,18 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         for(char c : s.toCharArray()) {
-            if(c >= 'A' && c <= 'Z') {
-                c = (char)('A' + (c + n - 'A')%26);
-            } else if(c >= 'a' && c <= 'z') {
-                c = (char)('a' + (c + n - 'a')%26);
-                
-            } else if(c == ' ') {
-                c = ' ';
+            if(c == ' ') {
+                sb.append(' ');
+                continue;
             }
-            answer += c;
+            if(c >= 'a' && c <='z') {
+                sb.append((char)('a' + (c + n - 'a')%26));
+            } 
+            else if(c >= 'A' && c <= 'Z') {
+                sb.append((char)('A' + (c + n - 'A')%26));
+            }
         }
-        return answer;
+        return sb.toString();
     }
 }
