cbee18d0adcbbaea1f0bf21f11f4888b7b96fbe7
SHIN YE RIN <nir2y@naver.com>
2026-02-16 14:57:27 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,16 +1,15 @@
 class Solution {
     public int solution(int[] number) {
-        int answer = 0;
-        int n = number.length;
-        for(int i=0; i<n-2; i++) {
-            for(int j=i+1; j<n-1; j++) {
-                for(int k=j+1; k<n; k++) {
+        int cnt = 0;
+        for(int i=0; i<number.length-2; i++) {
+            for(int j=i+1; j<number.length-1; j++) {
+                for(int k=j+1; k<number.length; k++) {
                     if(number[i] + number[j] + number[k] == 0) {
-                        answer++;
+                        cnt ++;
                     }
                 }
             }
         }
-        return answer;
+        return cnt;
     }
 }
