5a42b45f072d969c1915146f8298a68ae82ee3d8
SHIN YE RIN <nir2y@naver.com>
2026-02-16 14:35:28 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,19 +1,12 @@
 class Solution {
     public int[] solution(int n, int m) {
-        int[] answer = new int[2];
-        answer[0] = GCD(n, m);
-        answer[1] = LCM(n, m);
-        return answer;
+        return new int[]{gcd(n, m), lcm(n, m)};
     }
-    public int GCD(int n, int m) {
-        while(m != 0) {
-            int r = n % m;
-            n = m;
-            m = r;
-        }
-        return n;
+    private int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a%b);
     }
-    public int LCM(int n, int m) {
-        return n * m / GCD(n, m);
+    private int lcm(int a, int b) {
+        return a * b / gcd(a, b);
     }
 }
