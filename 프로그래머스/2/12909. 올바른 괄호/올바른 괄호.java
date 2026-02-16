128ad0371dbc66296b5aedb30ebf20a497764a60
SHIN YE RIN <nir2y@naver.com>
2026-02-16 14:09:44 +0900

    [level 2] Title: 올바른 괄호, Time: 21.57 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,17 @@ import java.util.Stack;
 
 class Solution {
     public boolean solution(String s) {
-        boolean flag = false;
         Stack<Character> stack = new Stack<>();
-        
-        for(char c : s.toCharArray()) {
-            if(c=='(') {
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '(') {
                 stack.push('(');
-            } else if(c == ')') {
-                if(!stack.isEmpty() && stack.peek() == '(') {
+            } else if(s.charAt(i) == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
                     stack.pop();
                 } else return false;
             }
         }
         if(stack.isEmpty()) return true;
-        return false;
+        return false;   
     }
 }
