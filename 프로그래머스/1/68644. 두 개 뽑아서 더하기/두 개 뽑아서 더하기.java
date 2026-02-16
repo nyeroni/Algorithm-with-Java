0059fc53c0a9481d34de58a05ab1c6c1c803f328
SHIN YE RIN <nir2y@naver.com>
2026-02-16 21:50:17 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.81 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -2,20 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer;
-        Set<Integer> tmp = new HashSet<>();
-        for(int i=0; i<numbers.length; i++) {
-            for(int j=0; j<numbers.length; j++) {
-                if(i != j) {
-                    tmp.add(numbers[i] + numbers[j]);
-                }
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<numbers.length-1; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
             }
         }
-        int i=0;
-        answer = new int[tmp.size()];
-        for(int t : tmp) {
-            answer[i++] = t;
-        }
+        int[] answer = set.stream().mapToInt(i->i).toArray();
         Arrays.sort(answer);
         return answer;
     }
