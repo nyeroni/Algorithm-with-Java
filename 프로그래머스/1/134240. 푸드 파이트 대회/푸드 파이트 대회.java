2e1268d30602304d6cb4bdf3f681c8d9f391a9dc
SHIN YE RIN <nir2y@naver.com>
2026-02-16 21:46:18 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.47 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,16 +1,14 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         for(int i=1; i<food.length; i++) {
             for(int j=0; j<food[i]/2; j++) {
-                answer += i;
+                sb.append(i);
             }
         }
-        String tmp = answer;
+        String answer = sb.toString();
         answer += "0";
-        StringBuffer sb = new StringBuffer(tmp);
-        tmp = sb.reverse().toString();
-        answer += tmp;
+        answer += sb.reverse().toString();
         return answer;
     }
 }
