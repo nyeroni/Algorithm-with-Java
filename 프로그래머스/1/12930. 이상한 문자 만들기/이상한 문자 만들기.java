affdbc42d2da7b46f9a90bc7a7c6084201e8bd56
SHIN YE RIN <nir2y@naver.com>
2026-02-16 15:30:30 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,21 +1,22 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
         int idx = 0;
         for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == ' ') {
-                answer += " ";
+            char c = s.charAt(i);
+            if(c == ' ') {
                 idx = 0;
+                sb.append(" ");
                 continue;
             }
-            String c = String.valueOf(s.charAt(i));
-            if(idx % 2 == 0) {
-                answer += c.toUpperCase();
+            if(idx%2==0) {
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(Character.toLowerCase(c));
             }
-            else answer += c.toLowerCase();
-            idx ++;
+            idx++;
         }
-
-        return answer;
+        return sb.toString();
     }
 }
