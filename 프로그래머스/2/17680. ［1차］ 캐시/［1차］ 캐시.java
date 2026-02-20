6592458005d4c83fe796d2baeef0eec7eb4d30ab
SHIN YE RIN <nir2y@naver.com>
2026-02-20 20:24:26 +0900

    [level 2] Title: [1차] 캐시, Time: 53.87 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,30 +1,29 @@
 import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
+        if(cacheSize == 0) return cities.length * 5;
         Queue<String> queue = new LinkedList<>();
-        if(cacheSize == 0) {
-            return cities.length * 5;
-        }
+        int answer = 0;
         for(int i=0; i<cities.length; i++) {
             cities[i] = cities[i].toLowerCase();
-        
-            if(queue.size() <= cacheSize ) {
-                if(queue.contains(cities[i])) {
-                    queue.remove(cities[i]);
-                    queue.offer(cities[i]);
+            if(queue.size() < cacheSize) {
+                if(!queue.isEmpty() && queue.contains(cities[i])) {
                     answer += 1;
+                    queue.remove(cities[i]);
                 } else {
                     answer += 5;
-                    if(queue.size() == cacheSize) {
-                        queue.poll();
-                        queue.offer(cities[i]);
-                    } else if(queue.size() < cacheSize) {
-                        queue.offer(cities[i]);
-                    }
+                } 
+                queue.add(cities[i]);
+            } else {
+                if(queue.contains(cities[i])) {
+                    answer += 1;
+                    queue.remove(cities[i]);
+                } else {
+                    answer += 5;
+                    queue.remove();
                 }
+                queue.add(cities[i]);
             }
-
         }
         return answer;
     }
