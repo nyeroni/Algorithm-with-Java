59dbf2f5c9ad6e9e759cb61fe53737bc76e53f45
SHIN YE RIN <nir2y@naver.com>
2026-02-20 16:01:54 +0900

    [level 2] Title: 할인 행사, Time: 108.13 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,20 +2,22 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int length = 0;
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<want.length; i++) {
-            map.put(want[i], number[i]);
-            length += number[i];
-        }
-        for(int i=0; i<=discount.length-length; i++) {
+        for(int i=0; i<discount.length; i++) {
             Map<String, Integer> tmp = new HashMap<>();
-            for(int j=i; j<i+length; j++) {
+            int count = 0;
+            for(int j=i; j<discount.length; j++) {
+                if(j-i >= 10 ) break;
                 tmp.put(discount[j], tmp.getOrDefault(discount[j], 0) + 1);
-                // System.out.println("discount[j] : " + discount[j] + "tmp.get(discount) : " + tmp.get(discount[j]));
             }
-            if(map.equals(tmp)) answer ++;
+            for(int j=0; j<want.length; j++) {
+                String s = want[j];
+                if(tmp.containsKey(s) && tmp.get(s) >= number[j]) {
+                    count ++;
+                }
+            }
+            if(count == want.length) answer ++;
         }
+        if(answer == 0) return 0;
         return answer;
     }
 }
