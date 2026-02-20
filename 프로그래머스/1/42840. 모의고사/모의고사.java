26a1ccc6193553ad37bfb5a23769f3fb065492aa
SHIN YE RIN <nir2y@naver.com>
2026-02-20 19:41:25 +0900

    [level 1] Title: 모의고사, Time: 3.84 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,37 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer;
         int[] one = {1, 2, 3, 4, 5};
         int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] score = new int[3];
+        int answer1 = 0, answer2 = 0, answer3 = 0;
         for(int i=0; i<answers.length; i++) {
-            if(answers[i] == one[i % one.length]) {
-                score[0]++ ;
+            if(one[i%5] == answers[i]) {
+                answer1++;
             }
-            if(answers[i] == two[i%two.length]) 
-            {
-                score[1] ++;
-            } 
-            if(answers[i] == three[i%three.length]) {
-                score[2] ++;
+            if(two[i%8] == answers[i]) {
+                answer2++;
             }
-        }
-        int max = score[0];
-        for(int i=0; i<score.length; i++) {
-            if(max < score[i]) {
-                max = score[i];
+            if(three[i%10] == answers[i]) {
+                answer3++;
             }
         }
-        
         List<Integer> list = new ArrayList<>();
-        for(int i=0; i<score.length; i++) {
-            if(max == score[i]) {
-                list.add(i+1);
-            }
-        }
-        answer = list.stream().mapToInt(i->i).toArray();
-        return answer;
+        int max = Math.max(answer1, answer2);
+        max = Math.max(max, answer3);
+        if(max == answer1) list.add(1);
+        if(max == answer2) list.add(2);
+        if(max == answer3) list.add(3);
+
+        return list.stream().mapToInt(i->i).toArray();
     }
 }
