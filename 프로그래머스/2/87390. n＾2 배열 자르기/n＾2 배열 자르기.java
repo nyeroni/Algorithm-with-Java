7f8626d89c6e9411a152a079eb7194abbb13af71
SHIN YE RIN <nir2y@naver.com>
2026-02-20 16:27:19 +0900

    [level 2] Title: n^2 배열 자르기, Time: 10.07 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,21 +1,37 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, long left, long right) {
-        // 00 -> 1
-        // 10 01 11 -> 2
-        // 20 21 22 02 12 -> 3
-        // 00 01 02 10 11 12 20 21 22
-        // 0. 1. 2. 3. 4. 5. 6. 7. 8
+        // 00 1 0
+        // 01 2 1
+        // 02 3 2
+        // 03 4 3
+        // 10 2 4
+        // 11 2 5
+        // 12 3 6
+        // 13 4 7
+        // 20 3 8
+        // 21 3 9
+        // 22 3 10
+        // 23 4 11
+        // 30 4 12
+        // 31 4 13
+        // 32 4 14
+        // 33 4 15
         
+        // 1234 2234 3334 4444
+        // 최대값 + 1 ??
+        int startA = (int)(left / (long)n);
+        int startB = (int)(left / (long)n);
+        int endA = (int)(right / (long)n);
+        int endB = (int)(right / (long)n);
+
         int[] answer = new int[(int)(right - left) + 1];
-        
         for(long i=left; i<=right; i++) {
-            int a = (int)(i/(long)n);
-            int b = (int)(i%(long)n);
-            int max = Math.max(a, b) + 1;
-            answer[(int)(i-left)] = max;
+            int a = (int)(i / (long)n);
+            int b =(int)(i % (long)n);
+            int max = Math.max(a, b);
+            answer[(int)(i-left)] = max + 1;
         }
         return answer;
-        
     }
 }
