0267af78f938146f200baf24eb48c55453c8b326
SHIN YE RIN <nir2y@naver.com>
2026-02-20 21:00:17 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,4 +1,2 @@
--- 코드를 입력하세요
-SELECT animal_type, ifnull(name, 'No name') as name, sex_upon_intake
-from animal_ins
-order by animal_id asc;
+SELECT ANIMAL_TYPE, IFNULL(NAME, 'No name'), SEX_UPON_INTAKE
+FROM ANIMAL_INS;
