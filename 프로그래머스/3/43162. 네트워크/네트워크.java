eb095f2f21cbbe4281b39acb5c27829b5f2ba6c1
SHIN YE RIN <nir2y@naver.com>
2026-02-21 09:07:42 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,21 +1,21 @@
 import java.util.*;
 class Solution {
+    static int answer = 0;
     public int solution(int n, int[][] computers) {
         boolean[] visited = new boolean[n];
-        int answer = 0;
         for(int i=0; i<n; i++) {
             if(!visited[i]) {
-                dfs(computers, i, visited);
+                dfs(computers, visited, i);
                 answer ++;
             }
         }
         return answer;
     }
-    private void dfs(int[][] computers, int node, boolean []visited) {
+    private void dfs(int[][]computers, boolean[] visited, int node) {
         visited[node] = true;
-        for(int i=0; i<computers.length; i++) {
+        for(int i=0; i<computers[node].length; i++) {
             if(!visited[i] && computers[node][i] == 1) {
-                dfs(computers, i, visited);
+                dfs(computers, visited, i);
             }
         }
     }
