82077dd0aa44ac62a6d9b15c5a6ae6dfe4fa1efe
SHIN YE RIN <nir2y@naver.com>
2026-02-26 15:56:35 +0900

    [level 2] Title: 롤케이크 자르기, Time: 309.41 ms, Memory: 185 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,20 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
+        int answer = 0;
         Map<Integer, Integer> mapA = new HashMap<>();
         Map<Integer, Integer> mapB = new HashMap<>();
-        int answer = 0;
         for(int i=0; i<topping.length; i++) {
-           mapB.put(topping[i], mapB.getOrDefault(topping[i], 0) + 1);
+            mapB.put(topping[i], mapB.getOrDefault(topping[i], 0) + 1);
         }
         for(int i=0; i<topping.length; i++) {
             mapA.put(topping[i], mapA.getOrDefault(topping[i], 0) + 1);
             if(mapB.get(topping[i]) == 1) {
                 mapB.remove(topping[i]);
-            } else  mapB.put(topping[i], mapB.getOrDefault(topping[i], 0) - 1);
-            if(mapA.size() == mapB.size()) {
-                answer ++;
+            } else {
+                mapB.put(topping[i], mapB.getOrDefault(topping[i], 0)-1);
             }
+            if(mapA.size() == mapB.size()) answer++;
         }
         return answer;
     }
