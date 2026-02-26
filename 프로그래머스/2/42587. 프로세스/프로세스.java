3aa402a5e39a5dc519f37b5d537730df0e13b832
SHIN YE RIN <nir2y@naver.com>
2026-02-26 17:07:36 +0900

    [level 2] Title: 프로세스, Time: 1.84 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,36 +2,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         Queue<int[]> queue = new LinkedList<>();
+        int answer = 0;
         for(int i=0; i<priorities.length; i++) {
             queue.offer(new int[]{i, priorities[i]});
         } 
-        boolean []visited = new boolean[priorities.length];
-        int[] tmp = new int[priorities.length];
-        int k=0;
         while(!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int idx = now[0];
-            int num = now[1];
-            visited[idx] = true;
+            int[] tmp = queue.poll();
             boolean flag = false;
-            for(int i=0; i<priorities.length; i++) {
-                if(!visited[i] && num < priorities[i]) {
+            for(int[] q : queue) {
+                if(tmp[1] < q[1]) {
                     flag = true;
+                    break;
                 }
             }
             if(flag) {
-                queue.offer(new int[]{idx, num});
-                visited[idx] = false; 
+                queue.offer(tmp);
             } else {
-                tmp[k] = idx;
-                k++;
-            }
-        }
-        int answer =0;
-        for(int i=0; i<tmp.length; i++) {
-            // System.out.println("tmp[i] : " +tmp[i]);
-            if(tmp[i] == location) {
-                answer = i+1;
+                answer ++;
+                if(tmp[0] == location) return answer;
             }
         }
         return answer;
