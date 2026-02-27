38d5eef32baac2156b990bfccde89b57f303249f
SHIN YE RIN <nir2y@naver.com>
2026-02-27 17:38:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.68 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,26 +3,28 @@ import java.util.*;
 class Solution {
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
+    static boolean[][] visited;
     public int solution(int[][] maps) {
+        int lenX = maps.length;
+        int lenY = maps[0].length;
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0, 1});
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        visited = new boolean[lenX][lenY];
         visited[0][0] = true;
         while(!queue.isEmpty()) {
             int[] tmp = queue.poll();
             int x = tmp[0];
             int y = tmp[1];
             int dist = tmp[2];
- 
-            if(x == maps.length-1 && y == maps[0].length-1) return dist;
+            if(x == lenX-1 && y == lenY-1) return dist;
             for(int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if(nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length) continue;
-
-                if(visited[nx][ny] || maps[nx][ny] == 0) continue;
-                visited[nx][ny] = true;
-                queue.offer(new int[]{nx, ny, dist + 1});
+                if(nx < 0 || nx >= lenX || ny < 0 || ny >= lenY) continue;
+                if(!visited[nx][ny] && maps[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    queue.offer(new int[]{nx, ny, dist + 1});
+                }
             }
         }
         return -1;
