f0cb7ba39636f41051bb96a60fa07def420b076a
SHIN YE RIN <nir2y@naver.com>
2026-02-28 21:16:51 +0900

    [level 1] Title: 소수 만들기, Time: 2.16 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,28 +1,19 @@
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        for(int i=0; i<nums.length-2; i++){
+        for(int i=0; i<nums.length-2; i++) {
             for(int j=i+1; j<nums.length-1; j++) {
                 for(int k=j+1; k<nums.length; k++) {
-                    if(isPrime(nums[i] + nums[j] + nums[k])) {
-                        answer ++;
-                    }
+                    int n = nums[i] + nums[j] + nums[k];
+                    if(isPrime(n)) answer ++;
                 }
             }
         }
-
         return answer;
     }
-    public boolean isPrime(int n) {
-        if(n==1) {
-            return false;
-        } else if(n==2 || n==3) {
-            return true;
-        }
+    private boolean isPrime(int n) {
         for(int i=2; i<=Math.sqrt(n); i++) {
-            if(n%i == 0) {
-                return false;
-            }
+            if(n%i == 0) return false;
         }
         return true;
     }
