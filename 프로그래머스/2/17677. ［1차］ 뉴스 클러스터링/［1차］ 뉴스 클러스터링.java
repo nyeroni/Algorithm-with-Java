375d313f46e7c06bf600a29c464a5a8c7d77d57b
SHIN YE RIN <nir2y@naver.com>
2026-02-28 23:15:54 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 13.51 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,72 +1,36 @@
 import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
+        double answer = 0;
         str1 = str1.toUpperCase();
         str2 = str2.toUpperCase();
-        if(str1.equals(str2)) {
-            return 65536;
-        }
-        List<String> str1Arr = makeArr(str1);
-        List<String> str2Arr = makeArr(str2);
-        
-        System.out.println("str1");
-        for(String s : str1Arr) {
-            System.out.println("s : " + s);
-        }
-        System.out.println("str2");
-        for(String s : str2Arr) {
-            System.out.println("s : " + s);
-        }
-        int answer[] = calculate(str1Arr, str2Arr);
-        
-        int uni = answer[0];
-        int inter = answer[1];
-        System.out.println("inter : " + inter);
-        System.out.println("uni : " + uni);
-
-        if(uni!=0) {
-            double ans = (double)inter / (double)uni;
-            System.out.println("ans : " + ans);
-            return (int)(ans * 65536.0);
-        }
-        return 0;
-    }
-    private int[] calculate(List<String> strA, List<String> strB) {
-        List<String> str1 = new ArrayList<>(strA);
-        List<String> str2 = new ArrayList<>(strB);
-        List<String> sum = new ArrayList<>();
-        List<String> mul = new ArrayList<>();
-        for(String s : str1) {
-            if(str2.contains(s)) {
-                str2.remove(s);
-                mul.add(s);
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();    
+        String s = "";
+        for(int i=0; i<str1.length()-1; i++) {
+            if(str1.charAt(i) >= 'A' && str1.charAt(i) <= 'Z' && str1.charAt(i+1) >= 'A' && str1.charAt(i+1) <= 'Z') {
+                s = String.valueOf(str1.charAt(i)) + String.valueOf(str1.charAt(i+1));
+                list1.add(s);
             }
-            sum.add(s);
         }
-        for(String s : str2) {
-            sum.add(s);
-        }
-        return new int[]{sum.size(), mul.size()};
-    }
-    private List<String> makeArr(String str) {
-        System.out.println("str : " + str);
-        List<String> strArr = new ArrayList<>();
-        for(int i=0; i<str.length()-1; i++) {
-  
-            if(!checkAlpa(str.charAt(i)) || !checkAlpa(str.charAt(i+1))) {
-                continue;
+        for(int i=0; i<str2.length()-1; i++) {
+            if(str2.charAt(i) >= 'A' && str2.charAt(i) <= 'Z' && str2.charAt(i+1) >= 'A' && str2.charAt(i+1) <= 'Z') {
+                s = String.valueOf(str2.charAt(i)) + String.valueOf(str2.charAt(i+1));
+                list2.add(s);
             }
-            
-            strArr.add(String.valueOf(str.charAt(i)) + String.valueOf(str.charAt(i+1))); 
         }
-        
-        return strArr;
-    }
-    
-    private boolean checkAlpa(char c) {
-        if((c>='a' && c<='z' ) || (c>='A' && c<='Z')) {
-            return true;
+        double a = 0, b = list1.size() + list2.size();
+        for(String ss : list2) {
+            if(list1.contains(ss)) {
+                // System.out.println("ss : " + ss);
+                list1.remove(ss);
+                a++;
+            }
         }
-        return false;
+        b -= a;
+        if(b == 0) answer = 1;
+        else answer = a / b;
+        System.out.println("a : " + a + ", b : " + b);
+        return (int)(answer * 65536.0);
     }
 }
