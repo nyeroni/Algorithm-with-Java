0e7f48cee12bf17e473c8250ef05edde9712aef5
SHIN YE RIN <nir2y@naver.com>
2026-04-03 13:12:02 +0900

    [level 1] Title: 실패율, Time: 6.52 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,33 +1,40 @@
 import java.util.*;
 class Solution {
+    static class Stage{
+        int number;
+        Double failRate;
+        
+        Stage(int number, Double failRate) {
+            this.number = number;
+            this.failRate = failRate;
+        }
+    }
     public int[] solution(int N, int[] stages) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
+        int[] stuck = new int[N+2];
+        for(int s : stages) {
+            stuck[s] ++;
+        }
+        // 0 1 3 2 1 0 1
+        int players = stages.length;
+        List<Stage> list = new ArrayList<>();
         for(int i=1; i<=N; i++) {
-            map.put(i, 0);
+            double failRate = 0;
+            if(players > 0) {
+                failRate = (double) stuck[i] / players;
+            }
+            list.add(new Stage(i, failRate));
+            players -= stuck[i];
         }
-        
-        for(int i=0; i<stages.length; i++) {
-            if(stages[i] <= N)
-                map.put(stages[i], map.getOrDefault(stages[i], 0) + 1);
-        }
-        double n = stages.length;
-        Map<Integer, Double> result = new HashMap<>();
-
-        for(int i=1; i<=N; i++) {
-            if(n==0) result.put(i, 0.0);
-            else result.put(i, ((double)map.get(i)/(double)n));
-            n-=map.get(i);
-        }
-        
-        List<Integer> list = new ArrayList<>(result.keySet());
-       
         list.sort((a, b) -> {
-            int cmp = Double.compare(result.get(b), result.get(a));
-            if(cmp != 0) return cmp;
-            return Integer.compare(a, b);
+            if(a.failRate == b.failRate) {
+                return a.number - b.number;
+            }
+            return Double.compare(b.failRate, a.failRate);
         });
-        
-        return list.stream().mapToInt(i->i).toArray();
+        int[] answer = new int[N];
+        for(int i=0; i<N; i++) {
+            answer[i] = list.get(i).number;
+        }
+        return answer;
     }
 }
