16154a482399a179e8fa8c081a4637724c7544d6
SHIN YE RIN <nir2y@naver.com>
2026-04-03 21:31:46 +0900

    [level 3] Title: 단어 변환, Time: 3.40 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -4,49 +4,43 @@ class Solution {
     static class Node {
         String word;
         int count;
-        
         Node(String word, int count) {
             this.word = word;
             this.count = count;
         }
     }
     public int solution(String begin, String target, String[] words) {
-        boolean exists = false;
-        for(String word : words) {
-            if(word.equals(target)) {
-                exists = true;
+        boolean flag = false;
+        for(String s : words) {
+            if(target.equals(s)) {
+                flag = true;
                 break;
             }
         }
-        if(!exists) return 0;
+        if(!flag) return 0;
         
-        Queue<Node> queue = new LinkedList<>();
         boolean[] visited = new boolean[words.length];
-        
+        Queue<Node> queue = new LinkedList<>();
         queue.offer(new Node(begin, 0));
-        
         while(!queue.isEmpty()) {
             Node current = queue.poll();
-            if(current.word.equals(target)) {
-                return current.count;
-            }
+            if(current.word.equals(target)) return current.count;
             for(int i=0; i<words.length; i++) {
-                if(!visited[i] && check(current.word, words[i])) {
-                    visited[i] = true;
+                if(!visited[i] && check(words[i], current.word)) {
                     queue.offer(new Node(words[i], current.count + 1));
                 }
             }
         }
         return 0;
     }
-    private boolean check(String str1, String str2) {
+    public boolean check(String a, String b) {
         int cnt = 0;
-        for(int i=0; i<str1.length(); i++) {
-            if(str1.charAt(i) == str2.charAt(i)) {
+        for(int i=0; i<a.length(); i++) {
+            if(a.charAt(i) == b.charAt(i)) {
                 cnt ++;
             }
         }
-        if(cnt == str1.length() - 1) return true;
+        if(cnt == a.length()-1) return true;
         return false;
     }
 }
