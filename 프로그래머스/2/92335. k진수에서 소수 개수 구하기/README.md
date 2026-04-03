e140b83b1d5d9c3c066a75bd07a9acb1c93ec31e
SHIN YE RIN <nir2y@naver.com>
2026-04-03 15:30:14 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 32.56 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 93.5 MB, 시간: 65.98 ms
+메모리: 87.5 MB, 시간: 32.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 01일 15:31:16
+2026년 04월 03일 15:30:11
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,24 +1,36 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        String formation = Integer.toString(n, k);
-        String[] tmp = formation.split("0+");
-        for(String t : tmp) {
-            double num = Double.parseDouble(t);
-            if(isPrime(num)) {
-                answer ++;
+        String str = Integer.toString(n, k);
+        System.out.println("str : " + str);
+        List<Long> list = new ArrayList<>();
+        String s = "";
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == '0') {
+                if(!s.equals("")) list.add(Long.parseLong(s));
+                s = "";
+            } else {
+                s += str.charAt(i);
             }
         }
+        if(!s.equals("")) list.add(Long.parseLong(s));
+        int answer = 0;
+        for(long num : list) {
+            if(isPrime(num)) answer ++;
+        }
         return answer;
+        
     }
-    public boolean isPrime(double n) {
-        if(n==1) return false;
-        if(n==2) return true;
-        for(double i=2; i<=Math.sqrt(n); i++) {
-            if(n%i == 0) {
+    private boolean isPrime(long num) {
+        if(num == 1) return false;
+        if(num == 2 || num == 3) return true;
+        for(int i=2; i<=Math.sqrt(num); i++) {
+            if(num%i == 0) {
                 return false;
             }
         }
         return true;
+        
     }
 }
