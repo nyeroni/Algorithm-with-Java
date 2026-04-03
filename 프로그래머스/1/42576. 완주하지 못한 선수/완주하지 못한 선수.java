353fa2f6b929031e337cb810379b88a6b9b70f3a
SHIN YE RIN <nir2y@naver.com>
2026-04-03 22:04:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.04 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,25 +2,16 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
         Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<completion.length; i++ ) 
-        {
-            String p = participant[i];
-            String c = completion[i];
+        for(String p : participant) {
             map.put(p, map.getOrDefault(p, 0) + 1);
-            map.put(c, map.getOrDefault(c, 0) - 1);
         }
-        String p = participant[participant.length-1];
-        
-        map.put(p, map.getOrDefault(p, 0) + 1);
-        
-        for(String str : map.keySet()) {
-            if(map.get(str) == 1) {
-                answer = str;
-            }
+        for(String c : completion) {
+            map.put(c, map.get(c) - 1);
         }
-        
-        return answer;
+        for(String key : map.keySet()) {
+            if(map.get(key) > 0) return key;
+        }
+        return "";
     }
 }
