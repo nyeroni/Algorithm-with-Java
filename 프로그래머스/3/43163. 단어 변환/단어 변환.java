5642f8be7ce8ff08c97f0f071e7187a13e88520d
SHIN YE RIN <nir2y@naver.com>
2026-04-03 13:36:21 +0900

    [level 3] Title: 단어 변환, Time: 0.95 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,52 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        boolean flag = false;
-        for(String s : words) {
-            if(s.equals(target)) flag = true;
+    static class Node {
+        String word;
+        int count;
+        
+        Node(String word, int count) {
+            this.word = word;
+            this.count = count;
         }
-        if(!flag) return 0;
-        Queue<String> queue = new LinkedList<>();
-        boolean[] visited = new boolean[words.length];
-        queue.offer(begin);
-        int answer = 0;
-        while(!queue.isEmpty()) {
-            String str = queue.poll();
-            System.out.println("str : " + str);
-            if(check(str, target)) {
-                answer ++;
+    }
+    public int solution(String begin, String target, String[] words) {
+        boolean exists = false;
+        for(String word : words) {
+            if(word.equals(target)) {
+                exists = true;
                 break;
             }
+        }
+        if(!exists) return 0;
+        
+        Queue<Node> queue = new LinkedList<>();
+        boolean[] visited = new boolean[words.length];
+        
+        queue.offer(new Node(begin, 0));
+        
+        while(!queue.isEmpty()) {
+            Node current = queue.poll();
+            if(current.word.equals(target)) {
+                return current.count;
+            }
             for(int i=0; i<words.length; i++) {
-                System.out.println("for");
-                if(visited[i]) continue;
-                if(check(str, words[i])) {
-                    queue.offer(words[i]);
-                    answer ++;
+                if(!visited[i] && check(current.word, words[i])) {
                     visited[i] = true;
-                    break;
+                    queue.offer(new Node(words[i], current.count + 1));
                 }
             }
         }
-        return answer;
+        return 0;
     }
-    private boolean check(String a, String b) {
+    private boolean check(String str1, String str2) {
         int cnt = 0;
-        for(int i=0; i<a.length(); i++) {
-            if(a.charAt(i) == b.charAt(i)) {
-                cnt++;
+        for(int i=0; i<str1.length(); i++) {
+            if(str1.charAt(i) == str2.charAt(i)) {
+                cnt ++;
             }
         }
-        if(cnt == a.length()-1) return true;
+        if(cnt == str1.length() - 1) return true;
         return false;
     }
 }
