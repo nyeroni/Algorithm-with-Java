b020fc809eec3aa1c64c37db47cc72eb2195879b
SHIN YE RIN <nir2y@naver.com>
2026-04-03 22:08:05 +0900

    [level 1] Title: 폰켓몬, Time: 6.37 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,11 +2,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        int len = nums.length;
+        
+        for(int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        int n = nums.length / 2;
-        if(map.size() >= n) return n;
-        return map.size();
+        int answer = 0;
+        answer = map.size();
+        if(answer > len/2) {
+            answer = len/2;
+        }
+        return answer;
     }
 }
