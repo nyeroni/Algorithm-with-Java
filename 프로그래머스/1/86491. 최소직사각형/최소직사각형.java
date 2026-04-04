0b17640e86be02fb3b739d1b87b19433a7a1ad9a
SHIN YE RIN <nir2y@naver.com>
2026-04-04 12:12:00 +0900

    [level 1] Title: 최소직사각형, Time: 1.66 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,18 +1,14 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int width = 0;
-        int height = 0;
-        for(int i=0; i<sizes.length; i++) {
-            int max = Math.max(sizes[i][0], sizes[i][1]);
-            int min = Math.min(sizes[i][0], sizes[i][1]);
-            
-            if(width < max) {
-                width = max;
-            } 
-            if(height < min) {
-                height = min;
-            }
+        int maxX = 0;
+        int maxY = 0;
+        for(int[] size : sizes) {
+            int max = Math.max(size[0], size[1]);
+            int min = Math.min(size[0], size[1]);
+            if(max > maxX) maxX = max;
+            if(min > maxY) maxY = min;
         }
-        return width * height;
+        
+        return maxX * maxY;
     }
 }
