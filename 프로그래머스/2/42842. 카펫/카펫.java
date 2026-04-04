7941d4dbf614a6582a2d33cbd98041f98dfed115
SHIN YE RIN <nir2y@naver.com>
2026-04-04 12:39:00 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,12 +1,23 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        for(int i=1; i<=Math.sqrt(yellow); i++) {
-            if(yellow % i == 0) {
-                if(i * 2 + (yellow/i + 2) * 2 == brown) {
-                    return new int[]{yellow/i + 2, i + 2};
-                }
+        // x * 2 + (y-2) * 2 = brown
+        // (x-2) * (y-2) = yellow
+        // x + y - 2 = brown/2
+        // x + y = brown/2 + 2
+        // xy -2x -2y + 4 = yellow
+        // xy = yellow - 4 + 2(x+y)
+        // xy = yellow - 4 + brown + 4
+        // xy = yellow + brown
+        int num = brown + yellow;
+        int x = 1;
+        for(int i=1; i<=Math.sqrt(num); i++) {
+            if(num % i == 0 && i + num/i - 2 == brown / 2) {
+                x = i;
+                break;
             }
-        }   
-        return new int[]{0, 0};
+        }
+        x = Math.max(x, num/x);
+        return new int[]{x, num/x};
+        
     }
 }
