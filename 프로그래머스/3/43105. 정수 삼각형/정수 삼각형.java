6ac97ddb6df42e8760aa30a70d73128746fb244e
SHIN YE RIN <nir2y@naver.com>
2026-04-05 13:12:07 +0900

    [level 3] Title: 정수 삼각형, Time: 10.02 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -6,17 +6,17 @@ class Solution {
             for(int j=0; j<triangle[i].length; j++) {
                 if(j == 0) {
                     triangle[i][j] += triangle[i-1][j];
-                } else if(j == triangle[i].length - 1) {
+                }
+                else if(j == triangle[i].length - 1) {
                     triangle[i][j] += triangle[i-1][j-1];
-                } 
-                else {
-                    triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+                } else {
+                   triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
                 }
             }
         }
         int answer = 0;
-        for(int num : triangle[triangle.length-1]) {
-            answer = Math.max(answer, num);
+        for(int i=0; i<triangle[triangle.length-1].length; i++) {
+            answer = Math.max(triangle[triangle.length-1][i], answer);
         }
         return answer;
     }
