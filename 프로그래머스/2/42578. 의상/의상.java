0f542dedcd709b1ad28d09a3ee475b4465c6584f
SHIN YE RIN <nir2y@naver.com>
2026-04-11 13:19:21 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
-        for(String[] cloth : clothes) {
-            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        for(String[] str : clothes) {
+            map.put(str[1], map.getOrDefault(str[1], 0) + 1);
         }
         int answer = 1;
-        for(String key : map.keySet()) {
-            answer *= (map.get(key) + 1);
+        for(String s : map.keySet()) {
+            answer *= (map.get(s) + 1);
         }
         return answer - 1;
     }
