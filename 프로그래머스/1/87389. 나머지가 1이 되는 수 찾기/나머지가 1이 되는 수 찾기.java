9742576934b1291d32eb0ea87546e0850d3f0b52
SHIN YE RIN <nir2y@naver.com>
2026-04-12 14:53:34 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 4.24 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -2,10 +2,9 @@ class Solution {
     public int solution(int n) {
         int x = 1;
         while(true) {
-            if(n%x==1) {
-                return x;
-            }
+            if(n%x == 1) break;
             x++;
         }
+        return x;
     }
 }
