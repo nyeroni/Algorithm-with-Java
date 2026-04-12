cf1f842dfd3219b75013915d3011fdc9b5979947
SHIN YE RIN <nir2y@naver.com>
2026-04-12 14:57:51 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-        int answer = 0;
+        int sum = 0;
         for(int i=0; i<absolutes.length; i++) {
-            if(!signs[i]) {
-                answer += (absolutes[i] * -1);
-            } else answer += absolutes[i];
+            if(signs[i]) sum += absolutes[i];
+            else sum -= absolutes[i];
         }
-        return answer;
+        return sum;
     }
 }
