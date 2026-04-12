aa009ea9a0a3ddee10ae136d499f2637222e5de2
SHIN YE RIN <nir2y@naver.com>
2026-04-12 14:13:18 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,9 +1,17 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i=1; i<=n; i++) {
-            if(n%i==0) answer += i;
+        if(n == 0) return 0;
+        else if(n == 1) return 1;
+        int answer = n + 1;
+        int s = (int) Math.sqrt(n);
+        for(int i=2; i<=s; i++) {
+            if(i * i == n && n % i == 0) answer += i;
+            else if(n % i == 0)  {
+                answer += i;
+                answer += n/i;
+            }
         }
+        
         return answer;
     }
 }
