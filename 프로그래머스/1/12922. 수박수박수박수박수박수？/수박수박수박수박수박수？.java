ff875ab221bb8032329830571ceafdef6ade6d17
SHIN YE RIN <nir2y@naver.com>
2026-04-12 15:15:40 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.00 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,13 +1,10 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<n; i++) {
-            if(i%2==0) {
-                answer += "수";
-            } else {
-                answer += "박";
-            }
+            if(i%2==0) sb.append("수");
+            else sb.append("박");
         }
-        return answer;
+        return sb.toString();
     }
 }
