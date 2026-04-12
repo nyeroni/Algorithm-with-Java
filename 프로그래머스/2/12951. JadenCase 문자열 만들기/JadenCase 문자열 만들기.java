b921023fd1a960960859a224258c7b240e961252
SHIN YE RIN <nir2y@naver.com>
2026-04-12 16:25:46 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,24 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int k=0;
         s = s.toLowerCase();
-        String answer = "";
-        int j = 0;
         for(int i=0; i<s.length(); i++) {
             if(s.charAt(i) == ' ') {
-                    answer += " ";
-                    j=0;
-                    continue;
-            }
-            if(j == 0) {
-                answer += String.valueOf((s.charAt(i))).toUpperCase();
-                j++;
-            } 
-            else {
-                answer += s.charAt(i);
-                j++;
-            }
+                sb.append(" ");
+                k=0;
+                continue;
+            } else if(k == 0 && s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                sb.append(String.valueOf(s.charAt(i)).toUpperCase());
+            } else sb.append(String.valueOf(s.charAt(i)));
+            k++;
         }
-        return answer;
+        return sb.toString();
     }
 }
