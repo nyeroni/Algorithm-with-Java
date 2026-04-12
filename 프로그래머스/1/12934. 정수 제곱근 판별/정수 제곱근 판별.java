c2c279d5b3e45e7c313579bad8ca1d9d89f5c69f
SHIN YE RIN <nir2y@naver.com>
2026-04-12 14:28:42 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.12 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -2,10 +2,8 @@ import java.util.*;
 
 class Solution {
     public long solution(long n) {
-        long x = (long) Math.sqrt(n);
-        if(x * x == n) {
-            return (x+1) * (x+1);
-        }
+        long l = (long)Math.sqrt(n);
+        if(l * l == n) return (l+1) * (l+1);
         return -1;
     }
 }
