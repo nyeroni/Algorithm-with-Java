22dff559615318a040400ade5f87d255500e44a1
SHIN YE RIN <nir2y@naver.com>
2026-04-13 17:48:53 +0900

    [level 2] Title: 귤 고르기, Time: 53.09 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -3,21 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<tangerine.length; i++) {
-            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        for(int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
-        int[] arr = new int[map.size()];
-        int j=0;
-        for(Integer num : map.keySet()) {
-            arr[j] = map.get(num);
-            j++;
-        }
-        Arrays.sort(arr);
-        int sum = 0, answer = 0;
-        for(int i=arr.length-1; i>=0; i--) {
-            sum += arr[i];
+        List<Integer> list = new ArrayList<>(map.values());
+        Collections.sort(list);
+        int answer = 0;
+        for(int i=list.size()-1; i>=0; i--) {
+            k-=list.get(i);
             answer ++;
-            if(sum >= k) break;
+            if(k <= 0) return answer;
         }
         return answer;
     }
