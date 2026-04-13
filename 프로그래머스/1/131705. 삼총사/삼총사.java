001a420834482534b0a26dd194d101cdb624ef80
SHIN YE RIN <nir2y@naver.com>
2026-04-13 13:48:02 +0900

    [level 1] Title: 삼총사, Time: 0.08 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -4,9 +4,11 @@ class Solution {
         dfs(number, 0, 0, 0);
         return count;
     }
-    public void dfs(int[] number, int start, int depth, int sum) {
+    public void dfs(int []number, int start, int depth, int sum) {
         if(depth == 3) {
-            if(sum == 0) count ++;
+            if(sum == 0) {
+                count ++;
+            }
             return;
         }
         for(int i=start; i<number.length; i++) {
