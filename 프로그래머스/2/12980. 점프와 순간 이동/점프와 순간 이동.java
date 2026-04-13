df66c24eba659a09e33b644e17a049cdaf5a4771
SHIN YE RIN <nir2y@naver.com>
2026-04-13 17:24:38 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(n>0) {
-            if(n%2==0) {
-                n/=2;
-            } if(n%2!=0) {
+        while(n > 0) {
+            if(n%2 != 0) {
+                n --;
                 answer ++;
-                n--;
+            } else {
+                n/=2;
             }
         }
         return answer;
