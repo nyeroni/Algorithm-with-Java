4f26332f5ddba2cdf09e613f078eebfd714f41bc
SHIN YE RIN <nir2y@naver.com>
2026-04-13 16:30:30 +0900

    [level 1] Title: 콜라 문제, Time: 0.14 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int a, int b, int n) {
+        int t = 0;
         int answer = 0;
-        while(true) {
-            System.out.println("n :" + n);
-            if(n < a) break;
-            int num = n/a;
-            int total = n - num * a;
-            total += b * num;
-            answer += b * num;
-            n = total;
+        while(n>=a) {
+            t = (n/a) * b;
+            answer += t;
+            t += n%a;
+            n = t;
         }
         return answer;
     }
