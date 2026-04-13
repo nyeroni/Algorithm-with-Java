0add9c54a54e633d5062e32215fae29bb4220e28
SHIN YE RIN <nir2y@naver.com>
2026-04-13 14:32:57 +0900

    [level 2] Title: 피보나치 수, Time: 4.07 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -7,8 +7,8 @@ class Solution {
         arr[0] = 0;
         arr[1] = 1;
         for(int i=2; i<=n; i++) {
-            arr[i] = arr[i-1]%1234567 + arr[i-2]%1234567;
+            arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
         }
-        return arr[n]%1234567;
+        return arr[n];
     }
 }
