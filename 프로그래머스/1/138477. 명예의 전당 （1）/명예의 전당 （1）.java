6269978861270321fff6a409b074d01ffd73585e
SHIN YE RIN <nir2y@naver.com>
2026-04-13 17:17:58 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.15 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,16 +1,14 @@
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
-        int[] answer = new int[score.length];
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[] answer = new int[score.length];
         for(int i=0; i<score.length; i++) {
             if(pq.size() < k) {
-                pq.add(score[i]);
-            } else {
-                if(score[i] > pq.peek()) {
-                    pq.remove();
-                    pq.add(score[i]);
-                }
+                pq.offer(score[i]);
+            } else if(pq.size() == k && pq.peek() < score[i]) {
+                pq.poll();
+                pq.offer(score[i]);
             }
             answer[i] = pq.peek();
         }
