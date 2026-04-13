e2a0b1e86d1b368e1b84979f2f597c1d186dafb7
SHIN YE RIN <nir2y@naver.com>
2026-04-14 00:48:18 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,16 +1,16 @@
 class Solution {
     public int solution(int[] arr) {
-        int tmp = arr[0];
+        int answer = arr[0];
         for(int i=1; i<arr.length; i++) {
-            tmp = lcm(tmp, arr[i]);
+            answer = LCM(answer, arr[i]);
         }
-        return tmp;
+        return answer;
     }
-    private int gcd(int a, int b) {
+    public int GCD(int a, int b) {
         if(b == 0) return a;
-        return gcd(b, a%b);
+        return GCD(b, a%b);
     }
-    private int lcm(int a, int b) {
-        return a * b / gcd(a, b);
+    public int LCM(int a, int b) {
+        return a * b / GCD(a, b);
     }
 }
