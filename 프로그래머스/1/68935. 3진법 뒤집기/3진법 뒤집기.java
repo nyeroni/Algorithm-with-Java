5f20a0afa333962a0da2caa8be29bc1abc3ac02b
SHIN YE RIN <nir2y@naver.com>
2026-04-13 13:53:19 +0900

    [level 1] Title: 3진법 뒤집기, Time: 15.43 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -3,9 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         String s = Integer.toString(n, 3);
-        StringBuilder sb = new StringBuilder(s);
-        s = sb.reverse().toString();
-        int answer = Integer.parseInt(s, 3);
+        String tmp = "";
+        for(int i=s.length()-1; i>=0; i--) {
+            tmp += s.charAt(i);
+        }
+        int answer = Integer.valueOf(tmp, 3);
         return answer;
     }
 }
