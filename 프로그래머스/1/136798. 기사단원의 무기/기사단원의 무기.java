3f4e00842f50dbb0fe9bbc83aa60ffe4ee157bde
SHIN YE RIN <nir2y@naver.com>
2026-04-15 10:23:09 +0900

    [level 1] Title: 기사단원의 무기, Time: 137.69 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -3,21 +3,20 @@ class Solution {
         int answer = 0;
         for(int i=1; i<=number; i++) {
             int n = count(i);
-            // System.out.println("n : " + n);
-            if(n <= limit) {
-                answer += n;
-            } else answer += power;
+            if(n <= limit) answer += n;
+            else answer += power;
         }
         return answer;
     }
-    private int count(int num) {
-        int answer = 0;
-        int mid = (int)Math.sqrt(num);
-        for(int i=1; i<=mid; i++) {
-            // System.out.println("i : " + i);
-            if(num % i == 0) answer += 2;
-        }
-        if(mid * mid == num) answer --;
-        return answer;
+    private int count(int n) {
+        int cnt = 0;
+        for(int i=1; i<=Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                cnt += 2;
+            }
+            if(i * i == n) cnt -= 1;
+
+        }        
+        return cnt;
     }
 }
