6c73ef36c6b3ef91317570db42bcb75ed3f4e3db
SHIN YE RIN <nir2y@naver.com>
2026-04-15 12:00:32 +0900

    [level 2] Title: 전화번호 목록, Time: 316.42 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-        for(int i=0; i<phone_book.length-1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) return false;
+        for(int i=1; i<phone_book.length; i++) {
+            if(phone_book[i].startsWith(phone_book[i-1])) return false;
         }
         return true;
     }
