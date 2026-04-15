e6fccac37085a5ca042ba13e316b2d3151e116ef
SHIN YE RIN <nir2y@naver.com>
2026-04-15 11:36:08 +0900

    [level 1] Title: 모의고사, Time: 3.75 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,27 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] a = {1, 2, 3, 4, 5};
-        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] one = {1, 2, 3, 4, 5};
+        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int x = 0, y = 0, z = 0;
-        for(int i=0; i<answers.length; i++) {
-           if(a[i%a.length] == answers[i]) {
-               x++;
-           }
-            if(b[i%b.length] == answers[i]) {
-               y++;
-           }
-            if(c[i%c.length] == answers[i]) {
-               z++;
-           }
-        }
         List<Integer> list = new ArrayList<>();
-        int max = Math.max(Math.max(x, y), z);
-        if(max == x) list.add(1);
-        if(max == y) list.add(2);
-        if(max == z) list.add(3);
+        int scoreOne = 0, scoreTwo = 0, scoreThree = 0;
+        for(int i=0; i<answers.length; i++) {
+            if(one[i%one.length] == answers[i]) scoreOne ++;
+            if(two[i%two.length] == answers[i]) scoreTwo ++;
+            if(three[i%three.length] == answers[i]) scoreThree ++;
+        }
+        int max = Math.max(scoreOne, Math.max(scoreTwo, scoreThree));
+        if(max == scoreOne) list.add(1);
+        if(max == scoreTwo) list.add(2);
+        if(max == scoreThree) list.add(3);
+        
         return list.stream().mapToInt(i->i).toArray();
     }
 }
