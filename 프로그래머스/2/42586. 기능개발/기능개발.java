12823e283edcfef0fa7ed68470223383198ff001
SHIN YE RIN <nir2y@naver.com>
2026-04-15 11:27:13 +0900

    [level 2] Title: 기능개발, Time: 3.11 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,39 +1,32 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> answer = new ArrayList<>();
+        int n = 0;
         Queue<Integer> queue = new LinkedList<>();
-        int ans = 0;
-        for(int i=0; i<progresses.length; i++) {
-            int num = 0;
-            if((100 - progresses[i]) % speeds[i] == 0) {
-                num = (100 - progresses[i]) / speeds[i];
+        List<Integer> list = new ArrayList<>();
+        int i = 0, cnt = 0;;
+        while(i < progresses.length) {
+            if((100 - progresses[i])%speeds[i] == 0) {
+                n = (100 - progresses[i])/speeds[i];
             } else {
-                num = (100 - progresses[i]) / speeds[i] + 1;
+                n = (100 - progresses[i])/speeds[i] + 1;
             }
-            System.out.println("num : " + num);
-            if(!queue.isEmpty() && queue.peek() < num) {
-                while(!queue.isEmpty()) {
-                    queue.poll();
-                    ans ++;
-                }
-                answer.add(ans);
-                ans = 0;
-                queue.offer(num);
+            if(!queue.isEmpty() && queue.peek() >= n) {
+                cnt++;
+            } else if(queue.isEmpty()) {
+                queue.offer(n);
+                cnt = 1;
             }
             else {
-                queue.offer(num);
-            }
-            
-        }
-        ans = 0;
-        if(!queue.isEmpty()) {
-            while(!queue.isEmpty()) {
+                list.add(cnt);
+                cnt = 1;
                 queue.poll();
-                ans ++;
+                queue.offer(n);
             }
+            i++;
         }
-        answer.add(ans);
-        return answer.stream().mapToInt(i->i).toArray();
+        
+        list.add(cnt);
+        return list.stream().mapToInt(j->j).toArray();
     }
 }
