b12ae5d71228393c371e1901aa3f69f6b125bceb
SHIN YE RIN <nir2y@naver.com>
2026-04-16 13:38:40 +0900

    [level 1] Title: 소수 찾기, Time: 209.32 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -2,14 +2,12 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         for(int i=2; i<=n; i++) {
-            if(isPrime(i)) {
-                answer ++;
-            }
+            if(countPrime(i)) answer ++;
         }
         return answer;
     }
-    private boolean isPrime(int n) {
-        for(int i=2; i<=Math.sqrt(n); i++) {
+    public boolean countPrime(int n) {
+        for(int i=2; i<=Math.sqrt(n); i++){
             if(n % i == 0) return false;
         }
         return true;
