ff83d67dcb33ff6e9b78a133b9f6eeb358fc5477
SHIN YE RIN <nir2y@naver.com>
2026-04-16 13:35:40 +0900

    [level 1] Title: 과일 장수, Time: 67.69 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,11 +1,9 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
-        
-        Arrays.sort(score);
-       // 1 1 1 2 2 3 3 
         int answer = 0;
-        for(int i=score.length-m; i>=0; i-= m) {
+        Arrays.sort(score);
+        for(int i=score.length-m; i>=0; i-=m) {
             answer += score[i] * m;
         }
         return answer;
