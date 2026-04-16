d2921b45278e77f2fe93c0b04140d4890913f15f
SHIN YE RIN <nir2y@naver.com>
2026-04-16 14:26:41 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 11.97 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,36 +1,39 @@
 import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
-        double answer = 0;
+        // J(A, B) : 두 집합의 교집합 크기 / 두 집합의 합집합 크기
+        // A나 B가 0인 경우 1
         str1 = str1.toUpperCase();
         str2 = str2.toUpperCase();
-        List<String> list1 = new ArrayList<>();
-        List<String> list2 = new ArrayList<>();    
-        String s = "";
+        List<String> s1 = new ArrayList<>();
+        List<String> s2 = new ArrayList<>();
         for(int i=0; i<str1.length()-1; i++) {
-            if(str1.charAt(i) >= 'A' && str1.charAt(i) <= 'Z' && str1.charAt(i+1) >= 'A' && str1.charAt(i+1) <= 'Z') {
-                s = String.valueOf(str1.charAt(i)) + String.valueOf(str1.charAt(i+1));
-                list1.add(s);
-            }
+            char c1 = str1.charAt(i);
+            char c2 = str1.charAt(i+1);
+            if(c1 < 'A' || c1 > 'Z' || c2 < 'A' || c2 > 'Z') continue;
+            s1.add(String.valueOf("" + c1 + c2));            
         }
         for(int i=0; i<str2.length()-1; i++) {
-            if(str2.charAt(i) >= 'A' && str2.charAt(i) <= 'Z' && str2.charAt(i+1) >= 'A' && str2.charAt(i+1) <= 'Z') {
-                s = String.valueOf(str2.charAt(i)) + String.valueOf(str2.charAt(i+1));
-                list2.add(s);
+            char c1 = str2.charAt(i);
+            char c2 = str2.charAt(i+1);
+            if(c1 < 'A' || c1 > 'Z' || c2 < 'A' || c2 > 'Z') continue;
+            s2.add(String.valueOf("" + c1 + c2));            
+        }
+        if(s1.size() == 0 && s2.size() == 0) return 65536;
+        // 교집합 : 
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        for(String s : s2) {
+            list2.add(s);
+        }
+        for(int i=0; i<s1.size(); i++) {
+            if(s2.contains(s1.get(i))) {
+                s2.remove(s1.get(i));
+                list1.add(s1.get(i));
+            } else {
+                list2.add(s1.get(i));
             }
         }
-        double a = 0, b = list1.size() + list2.size();
-        for(String ss : list2) {
-            if(list1.contains(ss)) {
-                // System.out.println("ss : " + ss);
-                list1.remove(ss);
-                a++;
-            }
-        }
-        b -= a;
-        if(b == 0) answer = 1;
-        else answer = a / b;
-        System.out.println("a : " + a + ", b : " + b);
-        return (int)(answer * 65536.0);
+        return (int) (((double) list1.size() / (double) list2.size()) * 65536);
     }
 }
