4dc7bc4bd724f7ea6cdde0735c0217cbfbbf18b6
SHIN YE RIN <nir2y@naver.com>
2026-04-16 12:36:02 +0900

    [level 1] Title: 덧칠하기, Time: 1.35 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,19 +1,11 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        boolean[] flag = new boolean[n+1];
-        for(int i=0; i<section.length; i++) {
-            flag[section[i]] = true;
-        }
-        for(int i=0; i<section.length; i++) {
-            if(flag[section[i]]) {
-                int num = section[i] + m ;
-                if(num > n) {
-                    num = n + 1;
-                }
-                for(int j=section[i]; j<num; j++) {
-                    flag[j] = false;
-                }
+        int start = section[0];
+        int end = start + m - 1;
+        int answer = 1;
+        for(int i=1; i<section.length; i++) {
+            if(section[i] > end) {
+                end = section[i] + m - 1;
                 answer ++;
             }
         }
