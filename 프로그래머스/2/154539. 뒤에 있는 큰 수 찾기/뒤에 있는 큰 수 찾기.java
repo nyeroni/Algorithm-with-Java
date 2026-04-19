62dfdb778d932cb20bd459cde93b6ed8f004a25c
SHIN YE RIN <nir2y@naver.com>
2026-04-20 01:25:59 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 92.27 ms, Memory: 203 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,12 +1,16 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
-        int []answer = new int[numbers.length];
+        int[] answer = new int[numbers.length];
         Arrays.fill(answer, -1);
         Stack<Integer> stack = new Stack<>();
         for(int i=0; i<numbers.length; i++) {
-            while(!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
-                answer[stack.pop()] = numbers[i];
+            while(!stack.isEmpty()) {
+                int idx = stack.peek();
+                if(numbers[i] > numbers[idx]) {
+                    answer[idx] = numbers[i];
+                    stack.pop();
+                } else break;
             }
             stack.push(i);
         }
