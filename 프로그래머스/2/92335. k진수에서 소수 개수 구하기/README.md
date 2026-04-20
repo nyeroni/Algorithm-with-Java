3152a8b5811a282a2b5caf14be8fdbde0488b401
SHIN YE RIN <nir2y@naver.com>
2026-04-21 00:12:34 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 15.87 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335#) 
 
 ### 성능 요약
 
-메모리: 87.5 MB, 시간: 32.56 ms
+메모리: 73.1 MB, 시간: 15.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 03일 15:30:11
+2026년 04월 21일 00:12:31
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -3,34 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k) {
         String str = Integer.toString(n, k);
-        System.out.println("str : " + str);
-        List<Long> list = new ArrayList<>();
-        String s = "";
-        for(int i=0; i<str.length(); i++) {
-            if(str.charAt(i) == '0') {
-                if(!s.equals("")) list.add(Long.parseLong(s));
-                s = "";
-            } else {
-                s += str.charAt(i);
+        System.out.println("str :" + str);
+        int answer = 0;
+        String[] array = str.split("0");
+        for(String s : array) {
+            if(!s.equals("") && isPrime(Long.parseLong(s))) {
+                answer ++;
             }
         }
-        if(!s.equals("")) list.add(Long.parseLong(s));
-        int answer = 0;
-        for(long num : list) {
-            if(isPrime(num)) answer ++;
-        }
         return answer;
-        
     }
-    private boolean isPrime(long num) {
+    public boolean isPrime(Long num) {
         if(num == 1) return false;
         if(num == 2 || num == 3) return true;
         for(int i=2; i<=Math.sqrt(num); i++) {
-            if(num%i == 0) {
+            if(num % i == 0) {
                 return false;
             }
         }
         return true;
-        
     }
 }
