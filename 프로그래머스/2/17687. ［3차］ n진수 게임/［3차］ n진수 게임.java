55ce81b6769f726d687278822d2245759f882fef
SHIN YE RIN <nir2y@naver.com>
2026-04-21 01:43:21 +0900

    [level 2] Title: [3차] n진수 게임, Time: 11.24 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -2,14 +2,20 @@ class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder sb = new StringBuilder();
         int num = 0;
-        while(sb.length() < t * m) {
-            sb.append(Integer.toString(num, n).toUpperCase());
-            num++;
+        int count = 0;
+        int total = 0;
+        while(count < t) {
+            String str = Integer.toString(num, n);
+            for(int i=0; i<str.length(); i++) {
+                if(count == t) break;
+                if(((total + i) % m) == (p-1)) {
+                    sb.append(String.valueOf(str.charAt(i)).toUpperCase());
+                    count ++;
+                }
+            }
+            total += str.length();
+            num ++;
         }
-        StringBuilder answer = new StringBuilder();
-        for(int i=p-1; answer.length() < t; i += m) {
-            answer.append(sb.charAt(i));
-        }
-        return answer.toString();
+        return sb.toString();
     }
 }
