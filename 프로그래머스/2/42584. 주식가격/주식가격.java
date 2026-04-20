d4a6b28245400b9112e49a2cb9263b8da964096d
SHIN YE RIN <nir2y@naver.com>
2026-04-21 01:28:05 +0900

    [level 2] Title: 주식가격, Time: 31.89 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,17 +4,17 @@ class Solution {
     public int[] solution(int[] prices) {
         Stack<Integer> stack = new Stack<>();
         int[] answer = new int[prices.length];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = answer.length - i - 1;
+        }
         for(int i=0; i<prices.length; i++) {
-            while(!stack.isEmpty() && prices[stack.peek()] > prices[i] ) {
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
                 int idx = stack.pop();
                 answer[idx] = i - idx;
             }
             stack.push(i);
         }
-        while(!stack.isEmpty()) {
-            int idx = stack.pop();
-            answer[idx] = prices.length - idx - 1;
-        }
+        
         return answer;
     }
 }
