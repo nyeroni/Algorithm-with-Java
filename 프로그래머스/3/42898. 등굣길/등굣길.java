6a04aa5344e777a994868543947650831b1feccd
SHIN YE RIN <nir2y@naver.com>
2026-04-22 22:49:39 +0900

    [level 3] Title: 등굣길, Time: 0.66 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,41 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        int MOD = 1000000007;
-        int [][] tmp = new int[n][m];
-        
-        for(int i=0; i<puddles.length; i++) {
-            tmp[puddles[i][1]-1][puddles[i][0]-1] = -1;
+        int[][] dp = new int[n+1][m+1];
+        boolean[][] visited = new boolean[n+1][m+1];
+        for(int[] puddle : puddles) {
+            visited[puddle[1]][puddle[0]] = true;
         }
-        
-        tmp[0][0] = 1;
-        for(int i=1; i<m; i++) {
-            if(tmp[0][i] == -1 || tmp[0][i-1] == -1) {
-                break;
-            }
-            else tmp[0][i] = 1;
-        }
-        for(int i=1; i<n; i++) {
-            if(tmp[i][0] == -1 || tmp[i-1][0] == -1) {
-                break;
-            }
-            else tmp[i][0] = 1; 
-        }
-        for(int i=1; i<n; i++) {
-            for(int j=1; j<m; j++){
-                if(tmp[i][j] == -1) {
-                    continue;
+        dp[1][1] = 1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(i == 1 && j == 1) {
+                     continue;
                 }
-                if(tmp[i-1][j] == -1) {
-                    tmp[i][j] = tmp[i][j-1];
-                } else if(tmp[i][j-1] == -1) {
-                    tmp[i][j] = tmp[i-1][j];
-                }
-                else {
-                    tmp[i][j] = (tmp[i-1][j] % MOD + tmp[i][j-1] % MOD) % MOD;
-                }
+                if(!visited[i][j]) {
+                    dp[i][j] = (dp[i-1][j] + dp[i][j-1])%1000000007;
+                } else dp[i][j] = 0;
             }
         }
-        return tmp[n-1][m-1]%MOD;
+        return dp[n][m];
     }
 }
