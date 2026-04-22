c5bfd40a0e8765b5409428b48822093652b8248e
SHIN YE RIN <nir2y@naver.com>
2026-04-23 01:34:45 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,42 +1,25 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
-        int win = 0;
-        int countZero = 0;
-        List<Integer> wins = new ArrayList<>();
-        for(int i : win_nums) {
-            wins.add(i);
+        List<Integer> win = new ArrayList<>();
+        for(int num : win_nums) {
+            win.add(num);
         }
+        int count = 0;
+        int zeroCount = 0;
         for(int i=0; i<lottos.length; i++) {
-            System.out.println("lottos[i] : " + lottos[i]);
-            if(wins.contains(lottos[i])) {
-                win ++;
-            }
-            if(lottos[i] == 0) {
-                countZero ++;
-            }
+            if(lottos[i] != 0 && win.contains(lottos[i])) {
+                count ++;
+            } else if(lottos[i] == 0) zeroCount ++;
         }
-        System.out.println("count :" + countZero);
-        System.out.println("win : " + win);
-        answer[0] = prize(win+countZero);
-        answer[1] = prize(win);
-        return answer;
+        return new int[]{calculate(count + zeroCount), calculate(count)};
     }
-    public int prize(int n) {
-        if(n == 6) {
-            return 1;
-        } else if(n==5) {
-            return 2;
-        }  else if(n==4) {
-            return 3;
-        }  else if(n==3) {
-            return 4;
-        }  else if(n==2) {
-            return 5;
-        } else {
-            return 6;
-        }
-        
+    public int calculate(int num ) {
+        if(num == 6) return 1;
+        else if(num == 5) return 2;
+        else if(num == 4) return 3;
+        else if(num == 3) return 4;
+        else if(num == 2) return 5;
+        return 6;
     }
 }
