76817d5bfb21707714c5a1aa5a9f2401522dff4a
SHIN YE RIN <nir2y@naver.com>
2026-04-22 21:05:38 +0900

    [level 3] Title: 야근 지수, Time: 128.92 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,17 +2,21 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        long answer = 0;
         for(int w : works) {
             pq.offer(w);
         }
-        for(int i=0; i<n; i++) {
-            int num = pq.poll();
-            pq.offer(num - 1);
+        while(!pq.isEmpty() && n > 0) {
+            int now = pq.poll();
+            if(!pq.isEmpty() && now >= pq.peek() || pq.isEmpty()) {
+                now --;
+                n--;
+            }
+            pq.offer(now);
         }
-        long answer = 0;
-        for(int num : pq) {
-            if(num < 0) num = 0;
-            answer += num * num; 
+        for(int p : pq) {
+            if(p < 0) p = 0;
+            answer += p * p;
         }
         return answer;
     }
