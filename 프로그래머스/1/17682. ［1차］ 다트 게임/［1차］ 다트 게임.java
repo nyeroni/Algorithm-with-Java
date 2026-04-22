626ba0f757ce11094b8669f4b93a1f42e8064974
SHIN YE RIN <nir2y@naver.com>
2026-04-23 01:24:06 +0900

    [level 1] Title: [1차] 다트 게임, Time: 10.56 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,55 +1,52 @@
 import java.util.*;
 class Solution {
     public int solution(String dartResult) {
+        // S = 1, D = 2, T = 3
+        // * -> 2배
+        // # -> * -1
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        List<Integer> list = new ArrayList<>();
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i<dartResult.length(); i++) {
+        while(i < dartResult.length()) {
             char c = dartResult.charAt(i);
-            // System.out.println("c : " + c );
             if(c >= '0' && c <= '9') {
-                if(i != dartResult.length() -1 && c == '1' && dartResult.charAt(i+1) == '0') {
-                    i++;
-                    stack.push(10);
-                } else {
-                    int n = Integer.parseInt(String.valueOf(c));
-                    stack.push(n); 
-                }
-            } else if(c == '*') {
-                if(stack.size() > 1) {
-                    int a = stack.pop();
-                    int b = stack.pop();
-                    
-                    b *= 2;
-                    a *= 2;
-                    
-                    stack.push(b);
-                    stack.push(a);
-                } else {
-                    int a = stack.pop();
-                    a *= 2;
-                    stack.push(a);
-                }
-            } else if(c == '#') {
-                int a = stack.pop();
-                a *= -1;
-                stack.push(a);
-            } else {
-                int s = stack.pop();
-                if(c == 'S') {
-                    stack.push(s);
-                } else if(c == 'D') {
-                    s = (int)Math.pow(s, 2);
-                    stack.push(s);
-                } else if(c == 'T') {
-                    s = (int)Math.pow(s, 3);
-                    stack.push(s);
-                } 
+                sb.append("" + c);
             }
+            else {
+                if(c == 'S') {
+                    int num = Integer.parseInt(sb.toString());
+                    list.add(num);
+                } else if(c == 'D') {
+                    int num = Integer.parseInt(sb.toString());
+                    list.add((int)Math.pow(num, 2));
+                } else if(c == 'T') {
+                    int num = Integer.parseInt(sb.toString());
+                    list.add((int)Math.pow(num, 3));
+                } else if(c == '*') {
+                    if(list.size() > 1) {
+                        int a = list.get(list.size() - 2) * 2;
+                        int b = list.get(list.size() - 1) * 2;
+                        list.remove(list.size() - 2);
+                        list.remove(list.size() - 1);
+                        list.add(a);
+                        list.add(b);
+                    } else {
+                        int a = list.get(list.size()-1) * 2;
+                        list.remove(list.size()-1);
+                        list.add(a);
+                    }
+                } else if(c == '#') {
+                    int a = list.get(list.size()-1) * (-1);
+                    list.remove(list.size()-1);
+                    list.add(a);
+                }
+                sb = new StringBuilder();
+            }
+            i++;
         }
-        while(!stack.isEmpty()) {
-            int n = stack.pop();
-            System.out.println("n : " + n);
-            answer += n;
+        for(int l : list) {
+            answer += l;
         }
         return answer;
     }
