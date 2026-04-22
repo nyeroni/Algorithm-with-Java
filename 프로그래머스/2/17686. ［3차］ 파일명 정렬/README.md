eb35e0cc8b4162cdfa29686b8974c949f1fa9630
SHIN YE RIN <nir2y@naver.com>
2026-04-23 02:07:19 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 20.81 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 파일명 정렬 - 17686 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686) 
 
 ### 성능 요약
 
-메모리: 84.3 MB, 시간: 82.67 ms
+메모리: 92.3 MB, 시간: 20.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 15:31:32
+2026년 04월 23일 02:07:15
 
 ### 문제 설명
 
--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,94 +1,57 @@
 import java.util.*;
 
 class Solution {
-    public String[] solution(String[] files) {
-        String[] answer;
-        List<String[]> strList = new ArrayList<>();
-        for(int i=0; i<files.length; i++) {
-            String [] tmp = new String[3];
-            String str = "";
-            int idx = -1;
-            boolean flag = false;
-            for(int j=0; j<files[i].length()-1; j++) {
-            
-                if(files[i].charAt(j) >= '0' && files[i].charAt(j) <= '9') {
-                
-                    if(!flag) {
-                        System.out.println("0.str : " + str);
-                        tmp[0] = str;
-                        flag = true;
-                        str = "";
-                    }
-                    if(files[i].charAt(j+1) < '0' || files[i].charAt(j+1) > '9') {
-                        str += String.valueOf(files[i].charAt(j));
-                        idx = j+1;
-                        System.out.println("idx : " + idx);
-                        break;
-                    }
-                    if(j+1 == files[i].length() -1) {
-                        str += String.valueOf(files[i].charAt(j));
-                       str += String.valueOf(files[i].charAt(j+1));                  
-                     
-                       break;
-                   }
-                }
-                else {
-                   if(j+1 == files[i].length() -1) {
-                      str += String.valueOf(files[i].charAt(j));
-                   System.out.println("0.str : " + str);
-                       tmp[0] = str;
-                       str = String.valueOf(files[i].charAt(j+1));                  
-                     
-                       break;
-                   }
-                }
-                str += String.valueOf(files[i].charAt(j));
-
-                System.out.println("str : " + str);
-            }
-            System.out.println("success");
-            System.out.println("1.str : " + str);
-            tmp[1] = str;
-            str = "";
-            if(idx == -1) {
-                tmp[2] = "";
-                strList.add(tmp);
-                continue;
-            }
-            if(idx < files[i].length()) {
-                System.out.println("iiidx = " + idx);
-                for(int j = idx; j<files[i].length(); j++) {
-                    str+= String.valueOf(files[i].charAt(j));
-                }
-            }
-            System.out.println("===Str : " + str) ;
-            tmp[2] = str;
-            strList.add(tmp);
+    static class File{
+        String fullName;
+        String head;
+        int num;
+        File(String fullName, String head, int num) {
+            this.fullName = fullName;
+            this.head = head;
+            this.num = num;
         }
-        System.out.println("size : " + strList.size());
-        strList.sort((a, b) -> {
-            String A = a[0].toLowerCase();
-            String B = b[0].toLowerCase();
-           int first = A.compareTo(B);
-            if(first != 0) {
-                return first;
+    }
+    public String[] solution(String[] files) {
+        List<File> list = new ArrayList<>();
+        for(String file : files) {
+            StringBuilder sb = new StringBuilder();
+            boolean flag = false;
+            String s = file.toLowerCase();
+            String head = "";
+            int num = 0;
+            for(char c : s.toCharArray()) {
+                if(c >= '0' && c <= '9') {
+                    if(!flag) {
+                        head = sb.toString();
+                        flag = true;
+                        sb = new StringBuilder();
+                    }
+                    sb.append(c + "");
+                } else if(!flag){
+                    sb.append(c + "");
+                } else if(flag){
+                    flag = false;
+                    num = Integer.parseInt(sb.toString());
+                    break;
+                }
+                if(flag) {
+                     num = Integer.parseInt(sb.toString());
+                }
             }
-             int second = Integer.compare(Integer.parseInt(a[1]), Integer.parseInt(b[1]));
-            return second;
-            
-            
+            list.add(new File(file, head, num));
+        }
+        Collections.sort(list, new Comparator<>(){
+           public int compare(File a, File b) {
+               if(a.head.equals(b.head)) {
+                   return a.num - b.num;
+               } 
+               return a.head.compareTo(b.head);
+           } 
         });
-        
-        String tmpStr = "";
-        int i=0;
-        answer = new String[strList.size()];
-        for(String[] s : strList) {
-            String ts = "";
-            for(String st : s) {
-                ts += st;
-            }
-            answer[i] = ts;
-            i++;
+        String[] answer = new String[list.size()];
+        int k=0;
+        for(File file : list) {
+            answer[k++] = file.fullName;
         }
         return answer;
     }
