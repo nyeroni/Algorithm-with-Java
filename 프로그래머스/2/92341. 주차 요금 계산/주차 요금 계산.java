de2b922785e372611c0f15dae67f1545084c0d5d
SHIN YE RIN <nir2y@naver.com>
2026-04-22 23:52:05 +0900

    [level 2] Title: 주차 요금 계산, Time: 23.11 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,94 +1,69 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        List<Integer> list = new ArrayList<>();
-        Map<String, String> map = new LinkedHashMap<>();
-        Map<String, Integer> timeMap = new HashMap<>();
-        for(int i=0; i<records.length; i++) {
-            String[] tmp = records[i].split(" ");
-            if(!map.containsKey(tmp[1])) {
-                if(tmp[2].equals("IN")) {
-                    map.put(tmp[1], tmp[0]);
-                }
-            }
-            else {
-                if(tmp[2].equals("OUT")) {
-                    String in = map.get(tmp[1]);
-                    map.remove(tmp[1]);
-                    String []hm1 = in.split(":");
-                    int h1 = Integer.parseInt(hm1[0]);
-                    int m1 = Integer.parseInt(hm1[1]);
-                    String []hm2 = tmp[0].split(":");
-                    int h2 = Integer.parseInt(hm2[0]);
-                    int m2 = Integer.parseInt(hm2[1]);
-                    int h=0, m=0;
-                    if(h2>h1) {
-                        h = h2 - h1;
-                    } 
-                    if(m2 > m1) {
-                        m = m2 - m1;
-                    } else {
-                        m = m2 + 60 - m1;
-                        h--;
-                    }
-                    m += h * 60;
-                    if(!timeMap.containsKey(tmp[1])) {
-                        timeMap.put(tmp[1], m);
-                    }
-                    else {
-                            timeMap.replace(tmp[1], timeMap.get(tmp[1]) + m);
-
-                        }
-                    }
+        // fees[0] : fees[1] -> fees[2] : fees[3]
+        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, String> carIn = new HashMap<>();
+        for(String record : records) {
+            String []tmp = record.split(" ");
+            int carNum = Integer.parseInt(tmp[1]);
+            if(tmp[2].equals("IN")) {
+                carIn.put(carNum, tmp[0]);
+            } else if(tmp[2].equals("OUT")) {
+                String[] inTime = carIn.get(carNum).split(":");
+                carIn.remove(carNum);
+                String[] outTime = tmp[0].split(":");
+                int h, m;
+                int oh = Integer.parseInt(outTime[0]);
+                int om = Integer.parseInt(outTime[1]);
+                int ih = Integer.parseInt(inTime[0]);
+                int im = Integer.parseInt(inTime[1]);
+                h = oh- ih;
+                if(om - im < 0) {
+                    m = om - im + 60;
+                    h--;
+                } else m = om - im;
+                int totalMin = h * 60 + m;
+                map.put(carNum, map.getOrDefault(carNum, 0) + totalMin);
             }
         }
-
-        for(String str : map.keySet()) {
-            String s = map.get(str);
-            String []hm1 = s.split(":");
-            int h1 = Integer.parseInt(hm1[0]);
-            int m1 = Integer.parseInt(hm1[1]);
-            if(h1==23 && m1 == 59) continue;
-            int h2 = 23;
-            int m2 = 59;
-            
-            int h=0, m=0;
-            if(h2>h1) {
-                h = h2 - h1;
-            } 
-            if(m2 > m1) {
-                m = m2 - m1;
-            } else {
-                m = m2 + 60 - m1;
+        for(int name : carIn.keySet()) {
+            System.out.println("name : " + name);
+            String[] inTime = carIn.get(name).split(":");
+            int h, m;
+            int oh = 23;
+            int om = 59;
+            int ih = Integer.parseInt(inTime[0]);
+            int im = Integer.parseInt(inTime[1]);
+            h = oh- ih;
+            if(om - im < 0) {
+                m = om - im + 60;
                 h--;
-            }
-            m += h * 60;
-            System.out.println("m :" + m);
-            if(!timeMap.containsKey(str)) {
-                timeMap.put(str, m);
-            }
-            else {
-                timeMap.replace(str, timeMap.get(str) + m);
-            }
+            } else m = om - im;
+            int totalMin = h * 60 + m;
+            map.put(name, map.getOrDefault(name, 0) + totalMin);
         }
-        System.out.println("map : " + timeMap.size());
-        List<String> keyList = new ArrayList<>(timeMap.keySet());
-        System.out.println("keyList : " + keyList.size());
-        Collections.sort(keyList);
-          for(String s : keyList) {
-                int m = timeMap.get(s);
-                int fee = calculate(fees, timeMap.get(s));
-                list.add(fee);
-            }
-        return list.stream().mapToInt(i->i).toArray();
-    }
-    private int calculate(int[] fees, int m) {
-        int fee = fees[1];
-        if(m > fees[0]) {
-            m = m-fees[0];
-            double count = Math.ceil(((double)m / (double)fees[2]));
-            fee += (int)(count * fees[3]);
+        for(int m : map.keySet()) {
+            int totalMin = map.get(m);
+            int pay = 0;
+            if(totalMin > fees[0]) {
+               totalMin = totalMin - fees[0];
+                pay += fees[1];
+                if(totalMin % fees[2] == 0) {
+                    pay += fees[3] * (totalMin / fees[2]);
+                } else pay += fees[3] * (totalMin / fees[2] + 1);
+            } else pay = fees[1];
+            map.put(m, pay);
+            System.out.println("pay : " + pay);
         }
-        return fee;
+       
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list);
+        int[] answer = new int[list.size()];
+        int j=0;
+        for(int k : list) {
+            answer[j++] = map.get(k);
+        }
+        return answer;
     }
 }
