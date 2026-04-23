54fc8ee2bf786004210988c9306264f350003c32
SHIN YE RIN <nir2y@naver.com>
2026-04-24 00:15:40 +0900

    [level 1] Title: 숫자 짝꿍, Time: 133.69 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,27 +1,23 @@
 import java.util.*;
 class Solution {
     public String solution(String X, String Y) {
-        String answer = "";
-        int []xCount = new int[10];
-        int []yCount = new int[10];
+        int[] countX = new int[10];
+        int[] countY = new int[10];
+        for(char c : X.toCharArray()) {
+            countX[c - '0'] ++;
+        }
+        for(char c : Y.toCharArray()) {
+            countY[c-'0'] ++;
+        }
         
-        for(int i=0; i<X.length(); i++) {
-            int idx = Integer.parseInt(String.valueOf(X.charAt(i)));
-            xCount[idx] ++;
-        }
-        for(int i=0; i<Y.length(); i++) {
-            int idx = Integer.parseInt(String.valueOf(Y.charAt(i)));
-            yCount[idx] ++;
-        }
         StringBuilder sb = new StringBuilder();
         for(int i=9; i>=0; i--) {
-            while(xCount[i] > 0 && yCount[i] > 0) {
-                    xCount[i] --;
-                    yCount[i] --;
-                    sb.append(i);
+            int count = Math.min(countX[i], countY[i]);
+            for(int j=0; j<count; j++) {
+                sb.append(i + "");
             }
         }
-        if("".equals(sb.toString())) return "-1";
+        if(sb.length() == 0) return "-1";
         if(sb.charAt(0) == '0') return "0";
         return sb.toString();
     }
