d108bbaaba548a509c32ee104f40c6a32e6f162f
SHIN YE RIN <nir2y@naver.com>
2026-04-23 13:36:46 +0900

    [level 3] Title: 단속카메라, Time: 15.49 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,22 +1,17 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
-        /*
-        -20 -15
-        -18 -13
-        -14 5
-        -5 -3
-        */
+       Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
         
-        int cnt = 1;
+        int answer = 1;
         int pos = routes[0][1];
-        for(int i=0; i<routes.length; i++) {
-            if(pos < routes[i][0]) {
-                cnt ++;
-                pos = routes[i][1];
+        
+        for(int[] route : routes){ 
+            if(pos < route[0]) {
+                pos = route[1];
+                answer++;
             }
         }
-        return cnt;
+        return answer;
     }
 }
