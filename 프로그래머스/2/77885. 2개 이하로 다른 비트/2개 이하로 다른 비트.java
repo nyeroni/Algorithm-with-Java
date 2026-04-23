110f32678d37802ea4ca4ff3970968da994cdef2
SHIN YE RIN <nir2y@naver.com>
2026-04-24 01:27:42 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 4.67 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -4,31 +4,12 @@ class Solution {
         long[] answer = new long[numbers.length];
         int idx = 0;
         for(long num : numbers) {
-            String n = Long.toString(num, 2);
-            String next = Long.toString(num+1, 2);
-            //100 101
-            
-            if(n.length() == next.length()) {
-                if(n.charAt(n.length()-1) == '0') {
-                    answer[idx] = num + 1;
-                }
-                else {
-                    int pos = n.lastIndexOf("01");
-                    String binary = n.substring(0, pos);
-                    binary += "10";
-                    if(binary.length() < n.length()) {
-                        binary += n.substring(pos+2, n.length());
-                    }
-                    answer[idx] = Long.parseLong(binary, 2);
-                }
+            if(num % 2 == 0) {
+                answer[idx++] = num + 1;
             } else {
-                //111 1000
-                long pow = (long)Math.pow(2, n.length()-1);
-                answer[idx] = num + pow;
+                answer[idx++] = (num + 1) + ((num ^ (num+1)) >> 2);
             }
-            idx ++;
         }
-        
         return answer;
     }
 }
