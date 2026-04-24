3ab69bd97931a90253f325cee31042bfe963593f
SHIN YE RIN <nir2y@naver.com>
2026-04-25 01:34:32 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.97 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,34 +1,26 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        Map<Integer, Stack<Integer>> map = new HashMap<>(); 
-           
-        for (int j = 0; j < board[0].length; j++) {
-            map.put(j + 1, new Stack<>());
-        }
-        for(int i=board.length-1; i>=0; i--) {
-            for(int j=0; j<board[i].length; j++) {
-                if (board[i][j] != 0) {
-                    map.get(j + 1).push(board[i][j]);
-                }
-            }
-        }
+        int j = 0;
+        int count = 0;
         Stack<Integer> stack = new Stack<>();
         for(int i=0; i<moves.length; i++) {
-            if(!map.get(moves[i]).isEmpty()) {
-               int m = map.get(moves[i]).pop();
-                if(!stack.isEmpty()) {
-                    int n = stack.peek();
-                    if(n==m) {
+            j=0;
+            while(true) {
+                if(j < board[0].length-1 && board[j][moves[i]-1] == 0) {
+                    j++;
+                } 
+                else if(j <  board[0].length && board[j][moves[i]-1] != 0) {
+                    if(!stack.isEmpty() && stack.peek() == board[j][moves[i]-1]) {
                         stack.pop();
-                        answer += 2;
-                        continue;
+                        count += 2;
                     }
-                }
-                stack.push(m);
+                    else stack.push(board[j][moves[i]-1]);
+                    board[j][moves[i]-1] = 0;
+                    break;
+                } else break;
             }
         }
-        return answer;
+        return count;
     }
 }
