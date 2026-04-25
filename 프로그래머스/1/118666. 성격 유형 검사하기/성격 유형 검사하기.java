12b49ef0ef56784d3c2fd4d5e3f08e17a2901131
SHIN YE RIN <nir2y@naver.com>
2026-04-25 11:57:05 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.20 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,45 +1,60 @@
 import java.util.*;
 class Solution {
-    Map<String, Integer> map = new HashMap<>();
+    static Map<Character, Integer> map = new HashMap<>();
     public String solution(String[] survey, int[] choices) {
-        String answer = "";
-        
-        for(int i=0; i<survey.length; i++) {
-            calculate(survey[i], choices[i]);
+        // R/T C/F J/M A/N
+        // 1~7
+        for(int i=0; i<choices.length; i++) {
+            check(survey[i], choices[i]);
         }
-
-        if(map.getOrDefault("R", 0) < map.getOrDefault("T", 0)) {
-            answer += "T";
+        StringBuilder sb = new StringBuilder();
+        if(map.getOrDefault('R', 0) >= map.getOrDefault('T', 0)) {
+            sb.append("R");
         } else {
-            answer += "R";
+            sb.append("T");
         }
-        if(map.getOrDefault("C", 0) < map.getOrDefault("F", 0)) {
-            answer += "F";
-        } else answer += "C";
-        if(map.getOrDefault("J", 0) < map.getOrDefault("M", 0)) {
-            answer += "M";
-        } else answer += "J";
-        if(map.getOrDefault("A", 0) < map.getOrDefault("N", 0)) {
-            answer += "N";
-        } else answer += "A";
-        
-        return answer;
+        if(map.getOrDefault('C', 0) >= map.getOrDefault('F', 0)) {
+            sb.append("C");
+        } else {
+            sb.append("F");
+        }
+        if(map.getOrDefault('J', 0) >= map.getOrDefault('M', 0)) {
+            sb.append("J");
+        } else {
+            sb.append("M");
+        }
+        if(map.getOrDefault('A', 0) >= map.getOrDefault('N', 0)) {
+            sb.append("A");
+        } else {
+            sb.append("N");
+        }
+        return sb.toString();
     }
-    public void calculate(String s, int n) {
-        String a = String.valueOf(s.charAt(0));
-        String b = String.valueOf(s.charAt(1));
-        if(n == 1) {
-            map.put(a, map.getOrDefault(a, 0) + 3);
-        } else if(n == 2) {
-            map.put(a, map.getOrDefault(a, 0) + 2);
-        } else if(n == 3) {
-            map.put(a, map.getOrDefault(a, 0) + 1);
-        } else if(n == 5) {
-            map.put(b, map.getOrDefault(b, 0) + 1);
-        } else if(n == 6) {
-            map.put(b, map.getOrDefault(b, 0) + 2);
-        } else if(n == 7) {
-            map.put(b, map.getOrDefault(b, 0) + 3);
+    public void check(String s, int num) {
+        int first = 0, second = 0;
+        switch(num){
+            case 1:
+                first = 3;
+                break;
+            case 2: 
+                first = 2;
+                break;
+            case 3:
+                first = 1;
+                break;
+            case 5:
+                second = 1;
+                break;
+            case 6:
+                second = 2;
+                break;
+            case 7:
+                second = 3;
+                break;
+            default:
+                break;
         }
+        map.put(s.charAt(0), map.getOrDefault(s.charAt(0), 0) + first);
+        map.put(s.charAt(1), map.getOrDefault(s.charAt(1), 0) + second);
     }
 }
