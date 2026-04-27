909042c69d330a35bcf35256f7c94ca744923cfe
SHIN YE RIN <nir2y@naver.com>
2026-04-27 17:41:45 +0900

    [level 3] Title: 여행경로, Time: 0.87 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,35 +1,38 @@
 import java.util.*;
 import java.util.stream.*;
 class Solution {
-    static Map<String, List<String>> map;
-    static List<String> answer;
+    static List<String> answer = new ArrayList<>();
+    static boolean[] visited;
     public String[] solution(String[][] tickets) {
-        map = new HashMap<>();
-        answer = new ArrayList<>();
-        for(int i=0; i<tickets.length; i++) {
-            if(!map.containsKey(tickets[i][0])) {
-                map.put(tickets[i][0], new ArrayList<String>());
-            } 
-            map.get(tickets[i][0]).add(tickets[i][1]);
-        }
-        
-        for(String key : map.keySet()) {
-            Collections.sort(map.get(key), Collections.reverseOrder());
-        }
-        dfs("ICN");
-        
-        Collections.reverse(answer);
+        visited = new boolean[tickets.length];
+        Arrays.sort(tickets, (a, b) -> {
+           if(a[0].equals(b[0])) {
+               return a[1].compareTo(b[1]);
+           }
+            return a[0].compareTo(b[0]);
+        });
+        List<String> route = new ArrayList<>();
+        route.add("ICN");
+        dfs("ICN", tickets, route, 0);
         return answer.toArray(new String[0]);
     }
-    public void dfs(String cur) {
-
-        if(map.containsKey(cur)) {
-            List<String> list = map.get(cur);
-            while(!list.isEmpty()) {
-                String tmp = list.remove(list.size()-1);
-                dfs(tmp);
+    public boolean dfs(String now, String[][] tickets, List<String> route, int depth) {
+        if(depth == tickets.length) {
+            answer = new ArrayList<>(route);
+            return true;
+        }
+        for(int i=0; i<tickets.length; i++) {
+            if(!visited[i] && tickets[i][0].equals(now)) {
+                visited[i] = true;
+                route.add(tickets[i][1]);
+                if(dfs(tickets[i][1], tickets, route, depth + 1)) {
+                    return true;
+                }
+                route.remove(route.size()-1);
+                visited[i] = false;
             }
         }
-        answer.add(cur);
+        return false;
+        
     }
 }
