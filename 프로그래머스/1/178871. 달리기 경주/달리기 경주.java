4a7ddefe5018e333d169e68dc0e240887c0b7605
SHIN YE RIN <nir2y@naver.com>
2026-04-27 23:33:26 +0900

    [level 1] Title: 달리기 경주, Time: 394.24 ms, Memory: 387 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -3,20 +3,21 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
         Map<String, Integer> map = new HashMap<>();
+        
         for(int i=0; i<players.length; i++) {
             map.put(players[i], i);
         }
-
         for(String c : callings) {
             int idx = map.get(c);
-            if(idx == 0) continue;
-            String prev = players[idx-1];
+            String front = players[idx - 1];
             players[idx-1] = c;
-            players[idx] = prev;
+            players[idx] = front;
             
-            map.put(c, idx -1);
-            map.put(prev, idx);
+            map.put(c, idx-1);
+            map.put(front, idx);
+            
         }
+      
         return players;
     }
 }
