4241a9bda751dbdda5cf244a308544370448b1ce
SHIN YE RIN <nir2y@naver.com>
2026-04-27 21:26:35 +0900

    [level 2] Title: 테이블 해시 함수, Time: 15.40 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -2,24 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
-        int answer = 0;
-        
-        Arrays.sort(data, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                if(a[col - 1] == b[col - 1]) {
-                    return b[0] - a[0];
-                } else {
-                    return a[col-1] - b[col-1];
-                }
+        Arrays.sort(data, (a, b) -> {
+            if(a[col-1] == b[col-1]) {
+                return b[0] - a[0];
             }
+            return a[col-1] - b[col-1];
         });
-        for(int i=row_begin-1; i<row_end; i++) {
+        int answer = -1;
+        for(int i=row_begin; i<=row_end; i++) {
             int sum = 0;
-            for(int j=0; j<data[i].length; j++) {
-                sum += (data[i][j]  % (i+1));
+            for(int d : data[i-1]) {
+                sum += d % i;
             }
-            answer ^= sum;
+            // System.out.println("sum : " + sum);
+            if(answer == -1) answer = sum;
+            else answer ^= sum;
         }
         return answer;
     }
