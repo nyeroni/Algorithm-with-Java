864cc41d00a3e69581d8279fc5cbee9722652c62
SHIN YE RIN <nir2y@naver.com>
2026-04-28 01:44:38 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 14.93 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,60 +1,69 @@
 import java.util.*;
 
 class Solution {
-    static List<Long> numbers = new ArrayList<>();
-    static List<String> operators = new ArrayList<>();
-    static String[][] priority = {
-        {"+", "-", "*"},
-        {"+", "*", "-"},
-        {"-", "+", "*"},
-        {"-", "*", "+"},
-        {"*", "+", "-"},
-        {"*", "-", "+"}
-    };
     public long solution(String expression) {
-        long answer = 0;
-        parse(expression);
-        for(String[] ops : priority) {
-            answer = Math.max(answer, Math.abs(calc(ops)));
-        }
-        return answer;
-    }
-    private void parse(String exp) {
-        numbers.clear();
-        operators.clear();
-        String num = "";
-        for(char c : exp.toCharArray()) {
-            if(c == '+' || c == '-' || c == '*') {
-                numbers.add(Long.parseLong(num));
-                num = "";
-                operators.add(Character.toString(c));
-            } else num += c;
-        }
-        numbers.add(Long.parseLong(num));
-    }
-    private long calc(String[] ops) {
-        List<Long> nums = new ArrayList<>(numbers);
-        List<String> opsList = new ArrayList<>(operators);
-        
-        for(String op : ops) {
-            for(int i=0; i<opsList.size();) {
-                if(opsList.get(i).equals(op)) {
-                    long a = nums.remove(i);
-                    long b = nums.remove(i);
-                    long res = operate(a, b, op);
-                    nums.add(i, res);
-                    opsList.remove(i);
-                } else i++;
+       // 숫자들과 3가지 연산문자(+, -, *)만으로 이루어짐
+        // 우선순위 자유롭게 재정의 -> 가장 큰 숫자!
+        // 다 다른 우선순위(동일한 순위 x)
+        // 음수라면 절대값 -> 가장 큰 숫자 ! 이게 상금임
+        // 100 - (200 * 300) - (500 + 20) = 100 - 60000 - 520 = |60420|
+        List<String> tmp = new ArrayList<>();
+        int idx = 0;
+        StringBuilder sb = new StringBuilder();
+        for(char c : expression.toCharArray()) {
+            if(c >= '0' && c <= '9') {
+                sb.append("" + c);
+            } else {
+                tmp.add(sb.toString());
+                sb = new StringBuilder();
+                tmp.add("" + c);
             }
         }
-        return nums.get(0);
+        tmp.add(sb.toString());
+        // for(String s : tmp) {
+        //     System.out.println("s : " + s);
+        // }
+        // System.out.println("========");
+        // tmp.add(0, "hi");
+        // tmp.remove(1);
+        // tmp.remove(1);
+        // tmp.remove(1);
+        // for(String s : tmp) {
+        //     System.out.println("s : " + s);
+        // }
+        long max = calculate("+", "-", "*", tmp);
+        max = Math.max(calculate("+", "*", "-", tmp), max);
+        max = Math.max(calculate("-", "*", "+", tmp), max);
+        max = Math.max(calculate("-", "+", "*", tmp), max);
+        max = Math.max(calculate("*", "+", "-", tmp), max);
+        max = Math.max(calculate("*", "-", "+", tmp), max);
+        return max;
     }
-    private long operate(long a, long b, String op) {
-        switch(op) {
-            case "+": return a+b;
-            case "-": return a-b;
-            case "*": return a*b;
+    public long calculate(String a, String b, String c, List<String> tmp) {
+        List<String> list = new ArrayList<>(tmp);
+    
+        list = calcu(a, list);
+        list = calcu(b, list);
+        list = calcu(c, list);
+       return Math.abs(Long.parseLong(list.get(0)));
+    }
+    public List<String> calcu(String a, List<String> list) {
+        for(int i=1; i<list.size()-1; i++) {
+            
+            if(list.get(i).equals(a)) {
+                long result = cal(list.get(i), Long.parseLong(list.get(i-1)), Long.parseLong(list.get(i+1)));
+                list.add(i-1, "" + result);
+                list.remove(i);
+                list.remove(i);
+                list.remove(i);
+                i--;
+            }
         }
-        return 0;
+        return list;
+    }
+    public long cal(String a, long num1, long num2) {
+        if(a.equals("+")) return num1 + num2;
+        else if(a.equals("*")) return num1 * num2;
+        return num1-num2;
     }
 }
