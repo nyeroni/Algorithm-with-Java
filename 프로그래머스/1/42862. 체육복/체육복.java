dd608735621ac4737022b907132989aecc4d43b8
SHIN YE RIN <nir2y@naver.com>
2026-04-29 02:15:05 +0900

    [level 1] Title: 체육복, Time: 0.16 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,30 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        int[] student = new int[n+1];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=1; i<=n; i++) {
+            map.put(i, 1);
+        }
         for(int l : lost) {
-            student[l] --;
+            map.put(l, map.get(l) - 1);
         }
         for(int r : reserve) {
-            student[r] ++;
+            map.put(r, map.get(r) + 1);
         }
+     
         for(int i=1; i<=n; i++) {
-            if(student[i] < 0) {
-                if(i > 1 && student[i-1] > 0) {
-                    student[i-1]--;
-                    student[i] ++;
-                } else if(i < n && student[i+1] > 0) {
-                    student[i+1] --;
-                    student[i] ++;
+            // System.out.println("i : " + i + ", map.get(i) : " + map.get(i));
+            if(map.get(i) < 1) {
+                if(i - 1 > 0 && map.get(i-1) > 1) {
+                    // System.out.println("b : " + (i-1));
+                    map.put(i, map.get(i) + 1);
+                    map.put(i-1, map.get(i-1)-1);
                 }
+                else if(i < n && map.get(i+1) > 1) {
+                    // System.out.println("a : " + (i+1));
+                    map.put(i, map.get(i) + 1);
+                    map.put(i+1, map.get(i+1)-1);
+                } 
             }
         }
-        int answer = 0;
+        int cnt = 0;
         for(int i=1; i<=n; i++) {
-            if(student[i] >= 0) answer++;
+            if(map.get(i) >= 1) cnt ++;
         }
-        return answer;
+        return cnt;
     }
 }
