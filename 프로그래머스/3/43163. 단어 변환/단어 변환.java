b3b6583e513eb05a0c08b0fe3c2fb22daffe98d2
SHIN YE RIN <nir2y@naver.com>
2026-04-29 01:12:52 +0900

    [level 3] Title: 단어 변환, Time: 0.72 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,37 +1,34 @@
 import java.util.*;
 
 class Solution {
-    static class Word {
-        String word;
-        int depth;
-        Word(String word, int depth) {
-            this.word = word;
-            this.depth = depth;
+    static class Word{
+        String name;
+        int count;
+        Word(String name, int count) {
+            this.name = name;
+            this.count = count;;
         }
     }
     public int solution(String begin, String target, String[] words) {
-        boolean exists = false;
-        for(String word : words) {
-            if(word.equals(target)) {
-                exists = true;
-                break;
-            }
-        }
-        if(!exists) return 0;
-        
-        boolean[] visited = new boolean[words.length];
         Queue<Word> queue = new LinkedList<>();
         queue.offer(new Word(begin, 0));
+        boolean[] visited = new boolean[words.length];
+        int answer = 0;
+        boolean flag = false;
+        for(String w : words) {
+            if(w.equals(target)) flag = true;
+        }
+        if(!flag) return 0;
         
         while(!queue.isEmpty()) {
-            Word current = queue.poll();
-            if(current.word.equals(target)) {
-                return current.depth;
-            }
+            Word now = queue.poll();
+            // System.out.println("now.name : " + now.name + ", now.count : " + now.count + 1);
+            if(check(now.name, target)) return now.count + 1;
             for(int i=0; i<words.length; i++) {
-                if(!visited[i] && check(words[i], current.word)) {
+                if(!visited[i] && check(now.name, words[i])) {
+                    // System.out.println("words[i] : " + words[i]);
                     visited[i] = true;
-                    queue.offer(new Word(words[i], current.depth + 1));
+                    queue.offer(new Word(words[i], now.count + 1));
                 }
             }
         }
@@ -40,9 +37,7 @@ class Solution {
     public boolean check(String a, String b) {
         int cnt = 0;
         for(int i=0; i<a.length(); i++) {
-            if(a.charAt(i) == b.charAt(i)) {
-                cnt ++;
-            }
+            if(a.charAt(i) == b.charAt(i)) cnt ++;
         }
         if(cnt == a.length()-1) return true;
         return false;
