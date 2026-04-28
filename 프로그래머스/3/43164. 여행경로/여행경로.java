f591c3da05998865f6305714bf07dd89bdc5ca06
SHIN YE RIN <nir2y@naver.com>
2026-04-29 01:48:26 +0900

    [level 3] Title: 여행경로, Time: 0.68 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,38 +1,37 @@
 import java.util.*;
-import java.util.stream.*;
+
 class Solution {
-    static List<String> answer = new ArrayList<>();
     static boolean[] visited;
+    static List<String> answer;
     public String[] solution(String[][] tickets) {
         visited = new boolean[tickets.length];
+        answer = new ArrayList<>();
+        
         Arrays.sort(tickets, (a, b) -> {
-           if(a[0].equals(b[0])) {
-               return a[1].compareTo(b[1]);
-           }
+           if(a[0].equals(b[0])) return a[1].compareTo(b[1]);
             return a[0].compareTo(b[0]);
         });
         List<String> route = new ArrayList<>();
         route.add("ICN");
-        dfs("ICN", tickets, route, 0);
+        dfs("ICN", tickets, route);
         return answer.toArray(new String[0]);
     }
-    public boolean dfs(String now, String[][] tickets, List<String> route, int depth) {
-        if(depth == tickets.length) {
+    public boolean dfs(String current, String[][] tickets, List<String> route) {
+        if(route.size() == tickets.length + 1) {
             answer = new ArrayList<>(route);
             return true;
         }
         for(int i=0; i<tickets.length; i++) {
-            if(!visited[i] && tickets[i][0].equals(now)) {
-                visited[i] = true;
-                route.add(tickets[i][1]);
-                if(dfs(tickets[i][1], tickets, route, depth + 1)) {
-                    return true;
-                }
-                route.remove(route.size()-1);
-                visited[i] = false;
+            if(visited[i]) continue;
+            if(!tickets[i][0].equals(current)) continue;
+            visited[i] = true;
+            route.add(tickets[i][1]);
+            if(dfs(tickets[i][1], tickets, route)) {
+                return true;
             }
+            visited[i] = false;
+            route.remove(route.size()-1);
         }
         return false;
-        
     }
 }
