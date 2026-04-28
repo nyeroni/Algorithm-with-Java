1dadd2686af18aa238257c734180e238c6533915
SHIN YE RIN <nir2y@naver.com>
2026-04-29 02:31:03 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -2,23 +2,20 @@ class Solution {
     public int solution(String name) {
         int answer = 0;
         int len = name.length();
+        int move = len-1;
         for(int i=0; i<len; i++) {
             char c = name.charAt(i);
-            answer += Math.min(c - 'A', 1 + 'Z' - c);
-        }
-        
-        int move = len - 1;
-        for(int i=0; i<len; i++) {
-            int next = i + 1;
-            
+            int up = c-'A';
+            int down = 'Z' - c + 1;
+            answer += Math.min(up, down);
+            int next = i+1;
             while(next < len && name.charAt(next) == 'A') {
                 next ++;
             }
-            
             move = Math.min(move, i * 2 + len - next);
             move = Math.min(move, (len - next) * 2 + i);
+            
         }
-        answer += move;
-        return answer;
+        return answer + move;
     }
 }
