e2e753931133252ed112317f20ec0d3d003e96af
SHIN YE RIN <nir2y@naver.com>
2026-04-29 14:34:56 +0900

    [level 3] Title: 입국심사, Time: 106.24 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,22 +2,23 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        Arrays.sort(times);
         long left = 0;
-        long right = (long) times[times.length-1] * n;
+        Arrays.sort(times);
+        long right = n * (long)times[times.length-1];
+        long answer = 0;
         while(left <= right) {
-            long mid = (left + right)/2;
+            long mid = (left + right) / 2;
             long sum = 0;
-            for(int i=0; i<times.length; i++) {
-                sum += mid/times[i];
-                if(sum >= n) break;
+            for(int time : times) {
+                sum += mid / time;
+                // if(sum >= n) break;
             }
+            // System.out.println("sum : " + sum);
             if(sum >= n) {
                 answer = mid;
                 right = mid - 1;
             } else {
-                left = mid + 1;
+                left = mid + 1;                
             }
         }
         return answer;
