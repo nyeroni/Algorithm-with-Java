df580e4c35ea1ee8ccabbede3e35a3da786e9dd2
SHIN YE RIN <nir2y@naver.com>
2026-05-11 22:44:26 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 25.66 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,38 +1,43 @@
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int answer = level(diffs, times, limit);
-        return answer;
-    }
-    public long calculate(int[] diffs, int[] times, long limit, int level)  {
-            long time = 0;
-            int answer = 0;
-
-            for(int i=0; i<diffs.length; i++) {
-                if(diffs[i] <= level) {
-                    time += times[i];
-                } else {
-                    
-                    int fail = diffs[i] - level;
-                    int tmp = (times[i] + times[i-1]) * fail + times[i];
-                    time += tmp;
-                }
+        // n개의 퍼즐을 제한 시간 내 풀어야함
+        // 난이도와 소요시간
+        // 숙련도에 따라 틀리는 횟수가 달라짐
+        // diff : 현재 퍼즐의 난이도
+        // time_cur : 현재 퍼즐의 소요시간
+        // time_prev : 이전 퍼즐의 소요시간
+        // level : 당신의 숙련도
+        // diff <= level -> 퍼즐 안틀림 time_cur만큼 사용
+        // diff > level -> diff - level번 틀림 -> 틀릴때마다 time_cur 사용 + time_prev만큼 사용해 이전 퍼즐 다시 풀어야함
+        // 이전 퍼즐을 다시 풀 때 이전 퍼즐의 난이도에 상관없이 틀리지 않음
+        int answer = Integer.MAX_VALUE;
+        int right = 0;
+        int left = Integer.MAX_VALUE;
+        for(int diff : diffs) {
+            if(right < diff) {
+                right = diff;
             }
-        return time;
-    }
-    public int level(int[] diffs, int[] times, long limit) {
-        int min = 1;
-        int max = 100000;
-        
-        
-        while(min<=max) {
-            int level = (min + max) / 2;
-            long time = calculate(diffs, times, limit, level); 
-            if(time > limit) {
-                min = level + 1;
-            } else {
-                max = level - 1;
+            if(left > diff) {
+                left = diff;
             }
         }
-        return max+1;
+        while(left <= right) {
+            long time = 0;
+            int mid = (left + right)/2;
+            for(int i=0; i<diffs.length; i++) {
+                if(diffs[i] <= mid) {
+                    time += (long)times[i];
+                } else {
+                    time += (long)(times[i] + times[i-1]) * (diffs[i] - mid) + times[i];
+                }
+            }
+            if(time <= limit) {
+                answer = mid;
+                right = mid-1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return answer;
     }
 }
