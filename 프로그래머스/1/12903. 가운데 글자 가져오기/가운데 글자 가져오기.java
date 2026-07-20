51b25ba68b39621527a72ac4f44fbac90b5d630e
SHIN YE RIN <nir2y@naver.com>
2026-07-20 22:50:48 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,9 +1,10 @@
 class Solution {
     public String solution(String s) {
         int len = s.length();
-        if(len % 2 == 0) {
-            return String.valueOf("" + s.charAt(len/2-1) + s.charAt(len/2)) ;
+        if(len%2==0) {
+            s = s.substring(len/2-1, len/2+1);
         }
-        return String.valueOf(s.charAt(len/2));
+        else s = s.substring(len/2, len/2+1);
+        return s;
     }
 }
