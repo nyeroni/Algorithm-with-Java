1090772454691ba44e2fb8c2d78e451cee10bff1
SHIN YE RIN <nir2y@naver.com>
2026-07-20 22:32:54 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,12 +1,17 @@
 class Solution {
     public int solution(long num) {
-        int answer = 0;
+        int cnt = 0;
         while(num != 1) {
-            if(answer == 500) return -1;
-            if(num % 2 == 0) num/=2;
-            else num = num * 3 + 1;
-            answer ++;
+            cnt ++;
+            if(num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = num * 3 + 1;
+            }
+            if(cnt > 500) {
+                return -1;
+            }
         }
-        return answer;
+        return cnt;
     }
 }
